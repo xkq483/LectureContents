5597b16846f868d283752fb3b5aee42b940784c3
jjrk1231 <jjrk1231@naver.com>
2021-05-10 22:28:41 +0900

    [문성호]0510 숙제

---
+++ java/SunghoMoon/Quiz1/src/Quiz12.java
@@ -0,0 +1,9 @@
+public class Quiz12 public class PrintTest {
+    public static void main(String[] args) {
+        // 각자 자신의 이름을 작성하도록 한다.
+        System.out.println("문성호");{
+            int number;
+            byte number2 = 7;
+            System.out.println("nuber2=" + number2);
+        }
+}
