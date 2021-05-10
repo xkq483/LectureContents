641b6c8702b640f21ab270f1bd68315c85990a86
ekank <ekankr2@naver.com>
2021-05-10 22:07:01 +0900

    [임익환]5월 10일 숙제

---
+++ java/IkhwanIm/Test2/src/test2.java
@@ -0,0 +1,11 @@
+public class test2 {
+    public static void main(String[] args) {
+        int a = 2, b= 3;
+        float c = 2.3f, d= 3.5f;
+        double e = 2.3f, f= 3.5f;
+
+        System.out.println(a + b);
+        System.out.println(c + d);
+        System.out.println(e + f);
+    }
+}
