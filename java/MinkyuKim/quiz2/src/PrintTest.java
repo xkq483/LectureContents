d76cb6f5661ee24ca4b0d6b20292b40a87889a2c
kmg13579 <980steroids@naver.com>
2021-05-10 22:30:07 +0900

    [김민규] 숙제 : 사칙연산

---
+++ java/MinkyuKim/quiz2/src/PrintTest.java
@@ -0,0 +1,8 @@
+public class PrintTest {
+    public static void main(String[] args) {
+        byte num1 = 3;
+        byte num2 = 2;
+        System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+
+    }
+    }
