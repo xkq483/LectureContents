b5af19a3ffcb0387f4279b17b6d89972a67c9598
ej800 <junberk@naver.com>
2021-05-10 22:31:43 +0900

    [김은진]숙제

--- java/EunjinKim/Test/src/Test.java
+++ java/EunjinKim/Test/src/Test.java
@@ -1,5 +1,17 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("12345");
+
+        int num1 = 6, num2 = 4;
+        //float형 데이터를 저장할때는 반드시 뒤에 f를 붙여줘야 한다
+        //f를 붙이지 않으면 기본적으로 double 타입으로 인식
+        float fnum1 =3.3f, fnum2 = 4.2f, fnum3 = 5.2f;
+
+        System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+        System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+        System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+        System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+        System.out.printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+        System.out.printf("%f %% %f = %f\n", fnum2, fnum1, fnum2 % fnum1);
     }
 }
