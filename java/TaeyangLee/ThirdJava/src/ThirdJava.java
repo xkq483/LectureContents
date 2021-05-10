44d469dc177cd9364e39c6e6b90fff475326c171
rudghxodid <rudghxodid@naver.com>
2021-05-10 21:59:47 +0900

    [이태양]5월10일 숙제

--- java/TaeyangLee/ThirdJava/src/ThirdJava.java
+++ java/TaeyangLee/ThirdJava/src/ThirdJava.java
@@ -1,5 +1,14 @@
 public class ThirdJava {
     public static void main(String[] args) {
         System.out.println("1번문제");
+
+        int num =2 ,num2 =4;
+        float fnum1 = 3.3f,fnum2=4.2f;
+
+        System.out.printf("%d + %d = %d \n",num,num2,num+num2);
+        System.out.printf("%d - %d = %d \n",num,num2,num-num2);
+        System.out.printf("%d * %d = %d \n",num,num2,num*num2);
+        System.out.printf("%d %% %d = %d \n",num,num2,num%num2);
+
     }
 }
--- java/TaeyangLee/ThridJava/src/Quiz.java
+++
@@ -1,5 +0,0 @@
-public class Quiz {
-    public static void main(String[] args) {
-        System.out.println("깃 연습하기");
-    }
-}
