da99cfb3be9fbebe49c613e64670276c6f982756
Kimjoongyeon <jjjr69@naver.com>
2021-05-10 23:00:45 +0900

    2021.5.10 숙제입니다

---
+++ java/JoongyeonKim/Test/src/Quiz1.java
@@ -0,0 +1,21 @@
+//2021년5월10일 숙제
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        int num1=6, num2=4, num3=7;
+
+        System.out.println(num3);
+        System.out.println(2+4+8);
+        System.out.printf("%d + %d=%d\n", num1, num2, num1 + num2);
+        System.out.printf("%d - %d=%d\n", num1, num2, num1 - num2);
+        System.out.printf("%d * %d=%d\n", num1, num2, num1 * num2);
+        System.out.printf("%d / %d=%d\n", num1, num2, num1 / num2);
+        System.out.printf("%d %% %d=%d\n", num1, num2, num1 % num2);
+
+        float num4=2.7f, num5=1.2f;
+        System.out.println(num4+num5);
+        
+
+
+    }
+}
