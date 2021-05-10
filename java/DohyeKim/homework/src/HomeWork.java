483ca11e278fdb4bd0e81e8d68ee7d3565cd1729
Do-hye <kimdohye0728@gmail.com>
2021-05-10 23:32:40 +0900

    [김도혜]숙제 1일차 , float 변수쓰면 글자가 회색으로 변함. 원인 모름.

---
+++ java/DohyeKim/homework/src/HomeWork.java
@@ -0,0 +1,13 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        System.out.printf("%d + %d = %d\n",a,b,a+b);
+        System.out.printf("%d - %d = %d\n",a,b,a-b);
+        System.out.printf("%d * %d = %d\n",a,b,a*b);
+        System.out.printf("%d / %d = %d\n",a,b,a/b);
+        System.out.printf("%d %% %d = %d\n",a,b,a%b);
+        System.out.println("안녕");
+    }
+}
