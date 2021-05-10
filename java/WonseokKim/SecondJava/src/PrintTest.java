53e9ace4da60ce4c9d16cad0123dc1ff18315bd8
wsk0307 <wsk0307@naver.com>
2021-05-10 17:26:37 +0900

    [김원석]두 번째 자바 작업 + 깃(Git) 사본 저장 연습

--- java/WonseokKim/SecondJava/src/PrintTest.java
+++ java/WonseokKim/SecondJava/src/PrintTest.java
@@ -1,5 +1,6 @@
 public class PrintTest {
     public static void main(String[] args) {
-        System.out.println("WonSeokKim");
+        System.out.println("김원석");
+        System.out.println(" Hello Java ");
     }
 }
