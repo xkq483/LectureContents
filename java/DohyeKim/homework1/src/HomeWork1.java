c09cd85b442e32a966aa43cc997fbfc2b095e8f0
Do-hye <kimdohye0728@gmail.com>
2021-05-10 23:17:19 +0900

    [김도혜]숙제 1일차, float 변수 입력 시 에러나서 패스함.

---
+++ java/DohyeKim/homework1/src/HomeWork1.java
@@ -0,0 +1,13 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 5;
+
+        System.out.printf("%d+%d=%d\n",a,b,a+b);
+        System.out.printf("%d-%d=%d\n",a,b,a+b);
+        System.out.printf("%d*%d=%d\n",a,b,a+b);
+        System.out.printf("%d/%d=%d\n",a,b,a+b);
+        System.out.printf("%d %% %d=%d\n",a,b,a+b);
+        System.out.println("안녕");
+    }
+}
