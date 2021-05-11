029693b5683009b2c252c3fd36f9b00e9b4e9111
wsk0307 <wsk0307@naver.com>
2021-05-11 13:34:24 +0900

    0510 수업 내용

---
+++ java/WonseokKim/0510HW/src/HW0510.java
@@ -0,0 +1,12 @@
+public class HW0510 {
+    public static void main(String[] args) {
+        int a = 8, b = 4;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+    }
+}
--- java/WonseokKim/Test/src/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("이것은 Git 업로드를 위한 연습입니다.");
-    }
-}
