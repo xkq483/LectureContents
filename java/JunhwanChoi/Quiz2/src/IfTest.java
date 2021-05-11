fffe06f456fbf0d9d4c88c9f8fc03b9789bf127d
ChoiJunHwanzz <wnsghks884@gmail.com>
2021-05-11 18:16:52 +0900

    [최준환]Quiz <3,4,5,6,7>

---
+++ java/JunhwanChoi/Quiz2/src/IfTest.java
@@ -0,0 +1,15 @@
+// 숫자 3이 짝수인지 홀수인지 판별하는 코드 작성
+
+public class IfTest {
+    public static void main(String[] args) {
+
+        int num=3;
+
+        if(num/3==1)
+        {
+            System.out.println("홀수입니다.");
+        }
+
+
+    }
+}
---
+++ java/JunhwanChoi/Quiz2/src/IfWhileScannerTest.java
@@ -0,0 +1,10 @@
+//키보드 입력이 0이 들어오기 전까지는 계속 무한 반복하도록 프로그램을 작성합니다.
+//        입력하는 키보드 값이 짝수인지 홀수인지 지속적으로 판별하도록 프로그래밍 해봅시다!
+
+public class IfWhileScannerTest {
+
+    public static void main(String[] args) {
+
+
+    }
+}
