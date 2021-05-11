29e310a02c201857069572f817e837065599f758
silenc3502 <gcccompil3r@gmail.com>
2021-05-11 19:19:04 +0900

    3일차 - 구분을 위한 주석 추가

--- java/SanghoonLee/ThirdLecture/src/Prob345.java
+++ java/SanghoonLee/ThirdLecture/src/Prob345.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Prob345 {
     public static void main(String[] args) {
+        // 3번 문제
         // 숫자 3이 짝수인지 홀수인지 판별
         int num = 3;
 
@@ -13,6 +14,7 @@ public class Prob345 {
             System.out.println("이 숫자는 2의 배수가 아닙니다.(홀수)");
         }
 
+        // 4번 문제
         // 아무 숫자나 3개 입력 받아서 숫자의 합을 출력
         Scanner scan = new Scanner(System.in);
 
@@ -35,6 +37,7 @@ public class Prob345 {
         //    그러므로 문장 뒤에 숫자가 오고 연산을 하게 된다면 소괄호를 꼭 넣어주세요!
         System.out.println(num1 + num2 + num3 + "숫자 3개의 합은 = ");
 
+        // 5번 문제
         // 1 ~ 20까지의 숫자중 2의 배수만 출력
         int i = 2;
 
@@ -45,6 +48,7 @@ public class Prob345 {
             // i = i + 2;
         }
 
+        // 추가 설명
         // 여기서 A += B 는 아래와 같다.
         // A = A + B
         // A -= B
