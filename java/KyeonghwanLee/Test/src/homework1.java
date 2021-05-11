7cb9d77f7ea5d5c89ace70637e4160a2f7bc804a
airtrade7 <airtrade7@naver.com>
2021-05-11 18:58:55 +0900

    [이경환]3,4,5번퀴즈

--- java/KyeonghwanLee/Test/src/homework1.java
+++
@@ -1,6 +0,0 @@
-public class homework1 {
-    public static void main(String[] args) {
-        int num1 = 7, num2 = 9;
-        System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
-        System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
-}
