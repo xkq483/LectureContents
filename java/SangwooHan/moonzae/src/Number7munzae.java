953bcd43ecf9bb63fe18929b0a99f92cbf3818ef
bombom23 <tkddn5678@naver.com>
2021-05-11 20:39:16 +0900

    [3~8번문제입니다.]한상우.

--- java/SangwooHan/moonzae/src/Number7munzae.java
+++ java/SangwooHan/moonzae/src/Number7munzae.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
-
+//1 ~ 키보드로 입력 받은 숫자중 3의 배수를 판별하여 출력해봅시다.
 public class Number7munzae {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("숫자를 입력하세요");
-        int i = scan.nextInt();
+        int loop = scan.nextInt();
+        int i = 1;
 
-        while(i *2 %3 == 0){
-            System.out.println("3의배수가 맞습니다");
+        while(i <= loop){
+            if ()
+
+
+            }
+
 
         }
 
