658dec2e47105dca7953f203a0fb29f15942bd92
ms109420 <ms109420@naver.com>
2021-05-11 19:17:26 +0900

    [김도연] 0511강의

--- java/DoyeonKim/ThirdLecture/src/IfTestWithInputEnhance.java
+++ java/DoyeonKim/ThirdLecture/src/IfTestWithInputEnhance.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class IfTestWithInputEnhance {
+    //퀴즈4
         public static void main(String[] args) {
-            System.out.println("두 개 숫자를 입력 받아 비교해봅니다.");
+            System.out.println("세 개 숫자를 입력 받아 합해봅니다.");
             Scanner scan = new Scanner(System.in);
 
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -11,14 +12,14 @@ public class IfTestWithInputEnhance {
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = scan.nextInt();
 
-            if (num1 > num2) {
-                System.out.printf("%d > %d\n", num1, num2);
-            } else if (num1 < num2) {
-                System.out.printf("%d < %d\n", num1, num2);
-            } else {
-                System.out.printf("%d 와 %d는 같습니다!\n", num1, num2);
+            System.out.print("세 번째 숫자를 입력하세요: ");
+            int num3 = scan.nextInt();
+
+                //System.out.printf("%d + %d + %d = %d\n", num1, num2, num3 ,num1 + num2 + num3);
+            System.out.println("숫자 3개의 합은 =" + (num1 + num2 + num3));
+
 
             }
         }
-    }
+
 
