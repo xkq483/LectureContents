295b83ed2486177ec072564caccf5833dbb05c00
YoonYeongKim1 <03y20y@naver.com>
2021-05-12 13:39:31 +0900

    [김윤영]3일차 강의내용 복습

---
+++ java/YoonYeongKim/ThirdLecture/src/InputTest.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("아무 숫자나 입력해보세요: ");
+        int num = scan.nextInt();
+
+        System.out.println("당신이 입력한 숫자는 = "+ num);
+
+    }
+}
