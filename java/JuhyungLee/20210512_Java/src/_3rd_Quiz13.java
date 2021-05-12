f76f0642b3b8c5d7b9bdf8f333e12fa2e65040b7
spmen12 <spmen12@naver.com>
2021-05-12 20:59:53 +0900

    [이주형] 20210511 commint 안 했던 것들 + 20210512 퀴즈

--- java/JuhyungLee/20210512_Java/src/_3rd_Quiz13.java
+++ java/JuhyungLee/20210512_Java/src/_3rd_Quiz13.java
@@ -1,6 +1,15 @@
 public class _3rd_Quiz13 {
     public static void main(String[] args) {
         //Quiz 13. for, if 사용해서
-        // 1~20에서 3의 배수를 출력하는 프로그램 만들기.(if 사용 없이)
+        //1~30까지 숫자중 짞수와 홀수를 각각 모두 판정하는 프로그램 만들기
+
+        for(int i = 1; i <= 30; i++){
+            if (i % 2 == 0) {
+                System.out.println(i + "는 짝수");
+            } else {
+                System.out.println(i + "는 홀수");
+            }
+        }
     }
 }
+
