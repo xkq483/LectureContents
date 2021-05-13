982ac90383c3f02ec18cc41dd3dc153e31134c91
kmg13579 <980steroids@naver.com>
2021-05-14 05:00:37 +0900

    [김민규] 5일차 복습 + 퀴즈

--- java/MinkyuKim/4일차복습/src/z_Quiz16_sum.java
+++ java/MinkyuKim/4일차복습/src/z_Quiz16_sum.java
@@ -4,11 +4,10 @@ public class z_Quiz16_sum {
         //1 ~ 100까지 숫자중 11의 배수들의 합을 구해보자!
         int sum = 0;
         for(int i = 11 ; i <= 100 ; i += 11) {
-            sum += 11;
+            sum += i;
             System.out.println("sum = " + sum);
-
-
-
                 }
+
+        System.out.println("최종 합산 값 = " + sum);
             }
         }
