3fb55c9c6c965606980959bab20d51bebd35ed71
ethanlinje <ethanlinje@gmail.com>
2021-05-13 14:33:28 +0900

    [박세진] 15,16,17번 문제

--- java/SejinPark/ThirdLecture/src/ForIfTest.java
+++ java/SejinPark/ThirdLecture/src/ForIfTest.java
@@ -1,12 +1,12 @@
 public class ForIfTest {
     public static void main(String[] args) {
-            for(int i=1; i<=30; i++) {
-                if(i%2 == 0) {
-                    System.out.println(i+"는 짝수");
+        //1 ~ 100까지 숫자중 4의 배수만 출력해보자
+
+            for(int i=1; i<=100; i++) {        //⑴1~100까지 숫자를 for문으로 반복
+                if(i%4 == 0) {                 //⑵그 중 4로 나눴을때 나머지가 0이면 4의배수
+                    System.out.println(i);     //⑶4의배수를 출력함
                 }
-                else {
-                    System.out.println(i+"는 홀수");
-                }
+
             }
     }
 }
--- java/SejinPark/ThirdLecture/src/ForTest2.java
+++
@@ -1,8 +0,0 @@
-public class ForTest2 {
-    public static void main(String[] args) {
-        for(int i=3; i<=20; i+=3) {
-            System.out.println(i);
-        }
-
-    }
-}
