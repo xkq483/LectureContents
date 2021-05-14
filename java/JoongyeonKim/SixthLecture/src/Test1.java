fc68b916590c000c9885a172fd9c81e863227c56
Kimjoongyeon <jjjr69@naver.com>
2021-05-14 20:42:00 +0900

    [김중연] 25번문제 코드입니다

---
+++ java/JoongyeonKim/SixthLecture/src/Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+    public static void main(String[] args) {
+    int i = 0;
+    int j =0;
+    int a =0;
+        for(a = 0; a<=100; a+=2){
+        }
+        for(i = 0; i<=100; i+=11){
+            }
+        for(j = 0; j<=100; j+=5){
+
+        }
+        System.out.println("답:" + (i+a-j));
+
+    }
+}
