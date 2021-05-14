2a2ccd8358175c3ebb017e1b04600070d7373d60
limcholong <limcho1ong@naver.com>
2021-05-14 23:44:04 +0900

    6일차 : 퀴즈 25번

---
+++ java/CholongLim/Day6/src/Day6_Quiz1.java
@@ -0,0 +1,13 @@
+public class Day6_Quiz1 {
+    public static void main(String[] args) {
+//        25번
+        int Numx = 1;
+        int j;
+        for(int i = 1; i <= 100; i++ ) {
+
+
+
+        }
+
+    }
+}
