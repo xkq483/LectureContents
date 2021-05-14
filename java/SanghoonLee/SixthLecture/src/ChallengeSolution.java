d6320e55814e698b78a9289c3fe3d47487f24432
silenc3502 <gcccompil3r@gmail.com>
2021-05-14 16:56:12 +0900

    6일차: 노배열 중복 회피 Challenge 문제 답 + 숫자값들 final로 고정시킴

--- java/SanghoonLee/SixthLecture/src/ChallengeSolution.java
+++ java/SanghoonLee/SixthLecture/src/ChallengeSolution.java
@@ -1,5 +1,14 @@
 public class ChallengeSolution {
     public static void main(String[] args) {
+        final int FIRST_BIAS = 5;
+        final int SECOND_BIAS = 1;
+
+        final int FIRST_RANGE = 6;
+        final int FIRST_OFFSET = 5;
+
+        final int SECOND_RANGE = 4;
+        final int SECOND_OFFSET = 7;
+
         final int BIN = 1;
         int testBit = 0;
         int randNum;
@@ -11,48 +20,49 @@ public class ChallengeSolution {
         // 이 상황에서 되도록이면 0번 비트부터 순차적으로 활용하고 싶을것이고 중간에 비는 공간이 없길 원할 것임
 
         // 5 ~ 10 ---> 6개 A조
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < FIRST_RANGE; i++) {
             // 5 ~ 10
             // 실제 출력값은 5 ~ 10을 사용하되
             // 비트 연산에서는 0 ~ 5를 사용하자는 것!
-            randNum = (int)(Math.random() * 6 + 5);
+            randNum = (int)(Math.random() * FIRST_RANGE + FIRST_OFFSET);
 
             // 2^5, 2^6, 2^7, 2^8, 2^9, 2^10
             // 2^0, 2^1, 2^2, 2^3, 2^4, 2^5
-            while ((testBit & (BIN << (randNum - 5))) != 0) {
-                randNum = (int)(Math.random() * 6 + 5);
+            while ((testBit & (BIN << (randNum - FIRST_BIAS))) != 0) {
+                randNum = (int)(Math.random() * FIRST_RANGE + FIRST_OFFSET);
             }
 
             System.out.printf("5 ~ 10 randNum = %d\n", randNum);
 
-            testBit |= (BIN << (randNum - 5));
+            testBit |= (BIN << (randNum - FIRST_BIAS));
         }
 
         System.out.println("testBit = " + testBit);
 
         // 7 ~ 10 ---> 4개 B조
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < SECOND_RANGE; i++) {
             // 7 ~ 10
             // 출력되는 숫자는 7 ~ 10을 사용하고
             // 비트 연산에는 6 ~ 9를 사용하도록 해보자!
-            randNum = (int)(Math.random() * 4 + 7);
+            randNum = (int)(Math.random() * SECOND_RANGE + SECOND_OFFSET);
 
             // 2^7, 2^8, 2^9, 2^10
             // 2^6, 2^7, 2^8, 2^9
-            while ((testBit & (BIN << (randNum - 1))) != 0) {
-                randNum = (int)(Math.random() * 4 + 7);
+            while ((testBit & (BIN << (randNum - SECOND_BIAS))) != 0) {
+                randNum = (int)(Math.random() * SECOND_RANGE + SECOND_OFFSET);
             }
 
             System.out.printf("7 ~ 10 randNum = %d\n", randNum);
 
-            testBit |= (BIN << (randNum - 1));
+            testBit |= (BIN << (randNum - SECOND_BIAS));
         }
 
         System.out.println("testBit = " + testBit);
 
-        // 이번 추첨의 당첨자는 A조의 2번 고객님입니다 ~~!!~!
+        // 예로 추첨(랜덤) 시스템을 돌렸다고 가정함
+        // 이번 추첨의 당첨자는 A조의 2번(랜덤으로 나온값) 고객님입니다 ~~!!~!
         if ((testBit & (1 << 2)) != 0) {
-            System.out.println("당첨을 축하드립니다! 당신이 뽑은 번호는 " + (2 + 5));
+            System.out.println("당첨을 축하드립니다! 당신이 뽑은 번호는 A조 " + (2 + FIRST_BIAS));
         }
     }
 }
