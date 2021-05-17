d12cc183bc5e29122af62b9306c35079b7926dd1
KoJaeKwon <su_dal@naver.com>
2021-05-17 14:34:28 +0900

    [고재권]6일차 복습

---
+++ java/JaekwonKo/Dayclass6_/src/Quiz_25_27.java
@@ -0,0 +1,40 @@
+public class Quiz_25_27 {
+    public static void main(String[] args) {
+        //25번 문제
+
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 11 == 0 && 1 % 5 == 0 && i % 2 == 0) {
+                System.out.println("110의 배수 = " + i);
+            } else if (i % 11 == 0 && i % 5 == 0) {
+                System.out.println("55의 배수 = " + i);
+            } else if (i % 11 == 0 && i % 2 == 0) {
+                System.out.println("22의 배수 = " + i);
+            } else if (i % 5 == 0 && i % 2 == 0) {
+                System.out.println("10의 배수 = " + i);
+            } else if (i % 11 == 0) {
+                System.out.println("11의 배수 = " + i);
+                sum += i;
+            } else if (i % 5 == 0) {
+                System.out.println("5의 배수 = " + i);
+                sum -= i;
+            } else if (i % 2 == 0) {
+                System.out.println("2의 배수 = " + i);
+                sum += i;
+            }
+        }
+
+        System.out.println("최종값 : " + sum);
+
+        //27번 문제  변수3개만있으면 된데!!! (피보나치수열)
+
+
+
+
+
+
+
+
+
+    }
+}
