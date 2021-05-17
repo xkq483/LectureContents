6918a5c2439876558536e5933715ee128d489f1d
rudghxodid <rudghxodid@naver.com>
2021-05-18 00:56:39 +0900

    [이태양]5월17일 수업내용 복습

--- java/TaeyangLee/SixthLecture/src/ContinueTest.java
+++ java/TaeyangLee/SixthLecture/src/ContinueTest.java
@@ -5,9 +5,6 @@ public class ContinueTest {
                 continue;
                 //continue 를 만나면 아래쪽에 진행해야하는 코드가 있어도
                 // 무조건 for문의 맨 위로 이동
-                //그러므로 증감식이 진행된다
-                //짝수 일  때 다시 위로 올라가야해서 홀수만 출력이된다.
-
             }
             System.out.println("i = "+ i);
         }
--- java/TaeyangLee/SixthLecture/src/SwitchStringTest.java
+++ java/TaeyangLee/SixthLecture/src/SwitchStringTest.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class SwitchStringTest {
     public static void main(String[] args) {
-        System.out.println("문자열도 Switch에서 쓸수 있다 입력해보자 :");
+        System.out.println("문자열도 Switch에서 가능하다! :");
         Scanner sc = new Scanner(System.in);
 
         String str = sc.nextLine();
 
         switch (str) {
             case "hi":
-                System.out.println("안녕");
+                System.out.println("안녕 반가웡");
                 break;
             default:
                 System.out.println("아무것도아니야");
