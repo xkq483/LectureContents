5d1d68171e231d555540419a9a86a76fdfcc7511
jdy8739 <jdy8739@naver.com>
2021-05-17 11:38:48 +0900

    퀴즈 25번 복습 및 풀이

---
+++ java/DoyoungJeong/SixthLecture/src/Quiz27Repeat.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Quiz27Repeat {
+    public static void main(String[] args) {
+
+        int num = 1, num2 = 0, i=1, valuN=1;
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while (valuN!=0) { //while문으로 입력, 출력을 반복하려했는데 첫 번째는 정상 출력이 되는데 두 번째부터 이상하게 출력이 되네요;;
+
+            System.out.print("n번 째 값 입력 : ");
+            valuN = sc.nextInt();
+
+            for (i=1; i<500; i++) {
+                if (i == 1 || i % 2 == 1) {
+                    num = num2 + num;
+                    // System.out.println(num);
+                } else if (i == 2 || i % 2 == 0) {
+                    num2 = num + num2;
+                    // System.out.println(num2);
+                }
+                if (valuN == i) {
+                    if (i == 1 || i % 2 == 1) {
+                        System.out.print(i + "번 째 값은 : " + num + "\n");
+                    } else System.out.print(i + "번 째 값은 : " + num2 + "\n");
+                }
+
+            }
+
+        }
+        System.out.println("프로그램 종료");
+    }
+}
