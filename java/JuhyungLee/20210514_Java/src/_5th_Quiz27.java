8e49c9019d436b14993a224597917e9a949422e1
spmen12 <spmen12@naver.com>
2021-05-18 12:14:25 +0900

    [이주형] 20210517 수업자료

--- java/JuhyungLee/20210514_Java/src/_5th_Quiz27.java
+++ java/JuhyungLee/20210514_Java/src/_5th_Quiz27.java
@@ -10,23 +10,21 @@ public class _5th_Quiz27 {
 
         System.out.print("f(n)의 값을 구한다. n: ");
         int n = scan.nextInt();
-        int a= 1; // f(1)의 값
-        int b= 1; // f(2)의 값
+        int a = 1; // f(1)의 값
+        int b = 1; // f(2)의 값
         int sum = 0;
 
-            for(int i = 1; i<=(n-2); i++ ){
-                if(n == 1){
-                    System.out.println(a);
-                    break;
-                } else if(n == 2){
-                    System.out.println(b);
-                    break;
-                } else if(n>=3){
-                    sum=a+b;
-                    a+=b;
-                    b=a-b;
-                }
+        if (n <= 0) {
+            System.out.println("1 이상의 값을 기입하시오");
+        } else if (n < 3) {
+            System.out.println(a);
+        } else {
+            for (int i = 1; i <= n-2; i++) {
+                sum = a + b;
+                a = b;
+                b = sum;
             }
-        System.out.println(sum);
+            System.out.println(sum);
+        }
     }
 }
