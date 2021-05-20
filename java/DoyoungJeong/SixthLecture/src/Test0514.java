8b0ca12bc4909c9ccfd80df8bfb073683046ac60
jdy8739 <jdy8739@naver.com>
2021-05-20 12:33:07 +0900

    0520 복습, 질문

--- java/DoyoungJeong/SixthLecture/src/Test0514.java
+++ java/DoyoungJeong/SixthLecture/src/Test0514.java
@@ -3,28 +3,29 @@ import java.util.Scanner;
 public class Test0514 {
     public static void main(String[] args) {
         //챌린지문제
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("n번 째 값 입력 : ");
-        int valuN = sc.nextInt();
 
-        int num, num2, i=1;
-        while(i<100) {
-            num=1, num2=0, i=1;
-            if(i==1 || i%2==1) {
-                num = num2 + num;
- //               System.out.println(num);
-            } else if(i==2 || i%2==0) {
-                num2 = num + num2;
- //               System.out.println(num2);
-            }
-            if (i==valuN) {
-                if (i==1 || i%2==1) {
-                    System.out.print(i + "번 째 값은 : " + num + "\n");
-                } else System.out.print(i + "번 째 값은 : " + num2 + "\n");
-            }
-            i++;
-        } //풀긴했지만 시간이 다소 많이 걸렸습니다.
+        while(true) {
+            System.out.print("n번 째 값 입력 : ");
+            int valueN = sc.nextInt();
+
+            int i = 1, num = 1, num2 = 0;
+            while (i <= valueN) {
+                if (i == 1 || i % 2 == 1) {
+                    num = num2 + num;
+                    System.out.println(num);
+                } else if (i == 2 || i % 2 == 0) {
+                    num2 = num + num2;
+                    System.out.println(num2);
+                }
+                if (i == valueN) {
+                    if (i == 1 || i % 2 == 1) {
+                        System.out.print(i + "번 째 값은 : " + num + "\n");
+                    } else System.out.print(i + "번 째 값은 : " + num2 + "\n");
+                }
+                i++;
+            } //풀긴했지만 시간이 다소 많이 걸렸습니다.
+        }
 
     }
 }
