fd502dabf5ef38bec73f292decc875a7279ac1ba
limcholong <limcho1ong@naver.com>
2021-05-20 14:19:16 +0900

    8일차 : 문제 35번

--- java/CholongLim/Day8/src/Day8_Quiz35.java
+++ java/CholongLim/Day8/src/Day8_Quiz35.java
@@ -1,23 +1,31 @@
-class Dice2 {
-    int comDice2;
-    int userDice2;
+class Dice3 {
+    int comDice;
+    int userDice;
 
-    Dice2() {
-        comDice2 = randomdice();
-        userDice2 = randomdice();
+    Dice3() {
+        comDice = getRandomDice();
+        userDice  = getRandomDice();
     }
-
-    int randomdice() {
-        return (int) (Math.random() * 6 + 1);
+    int getRandomDice() {
+        return (int)(Math.random() * 6 + 1);
     }
-
-
 }
 
 public class Day8_Quiz35 {
     public static void main(String[] args) {
-        Dice2 t = new Dice2();
 
+        Dice3 dg = new Dice3();
+
+        if(dg.comDice > dg.userDice) {
+            System.out.printf("%d(컴퓨터) vs %d(사용자)\n",dg.comDice, dg.userDice);
+            System.out.println("컴퓨터의 승리입니다.");
+        } else if(dg.comDice < dg.userDice) {
+            System.out.printf("%d(컴퓨터) vs %d(사용자)\n",dg.comDice, dg.userDice);
+            System.out.println("사용자의 승리입니다.");
+        } else {
+            System.out.printf("%d(컴퓨터) vs %d(사용자)\n",dg.comDice, dg.userDice);
+            System.out.println("무승부입니다.");
+        }
     }
 }
 
