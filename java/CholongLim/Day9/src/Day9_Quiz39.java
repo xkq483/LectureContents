6514d613b01de5a64d4de54e515c3f0b5fc9fdd4
limcholong <limcho1ong@naver.com>
2021-05-21 14:36:52 +0900

    9일차 : 문제 39번 주사위 굴리기 ACL

--- java/CholongLim/Day9/src/Day9_Quiz39.java
+++ java/CholongLim/Day9/src/Day9_Quiz39.java
@@ -1,37 +1,38 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
 class DiceGame {
-    private int comDice;
-    private int userDice;
+
+    private int comDice2;
+    private int userDice2;
 
     public DiceGame() {
-        comDice = getRandomDice();
-        userDice = getRandomDice();
+         comDice2 = getRandomDice();
+         userDice2 = getRandomDice();
     }
 
-    private int getRandomDice() {
+    public int getRandomDice () {
         return (int) (Math.random() * 6 + 1);
     }
+    public void whoIsWinner() {
+        if (comDice2 > userDice2) {
+            System.out.printf("%d VS %d, 컴퓨터의 승리입니다.\n", comDice2, userDice2);
+        } else if (comDice2 < userDice2) {
+            System.out.printf("%d VS %d, 사용자의 승리입니다.\n", comDice2, userDice2);
+        } else {
+            System.out.printf("%d VS %d, 무승부입니다.\n", comDice2, userDice2);
+        }
+    }
+
+    public static class Day9_Quiz39 {
+        public static void main(String[] args) {
+            DiceGame dg = new DiceGame();
+            dg.whoIsWinner();
+
+        }
+    }
+}
+
+
+
 
 //    class 내부에서 만들어지는 코드들은 어떤 식으로 사용이던
 //    생성자이기때문에 그 방식으로 만들어지는듯 하다.
 //    따라서 조건문이나 추가문이 붙더라도 checkWinner()와 같은 형식으로 시작해야한다.
-
-    public void checkWinner() {
-        if (comDice > userDice) {
-            System.out.printf("%d(컴퓨터) VS %d(사용자), 컴퓨터 승리", comDice, userDice);
-        } else if (comDice < userDice) {
-            System.out.printf("%d(컴퓨터) VS %d(사용자), 사용자 승리", comDice, userDice);
-        } else {
-            System.out.printf("%d(컴퓨터) VS %d(사용자), 무승부", comDice, userDice);
-        }
-
-    }
-}
-public class Day9_Quiz39 {
-    public static void main(String[] args) {
-        DiceGame dg = new DiceGame();
-        dg.checkWinner();
-
-    }
-}
