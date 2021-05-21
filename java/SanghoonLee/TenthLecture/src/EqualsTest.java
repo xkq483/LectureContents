e2df86f88f69f1e7a8e228618c516b0c44671f71
silenc3502 <gcccompil3r@gmail.com>
2021-05-21 18:06:39 +0900

    10일차: 문자열 비교시에는 equals를 사용합니다.

---
+++ java/SanghoonLee/TenthLecture/src/EqualsTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EqualsTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        if (str.equals("네")) {
+            System.out.println("오 그래");
+        } else if (str.equals("아니오")) {
+            System.out.println("(처)맞는말");
+        } else {
+            System.out.println("무조건 동의하세요!");
+        }
+    }
+}
