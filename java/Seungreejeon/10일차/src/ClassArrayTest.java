6b626b214db9dfc386c7ff2bf4e7e287b44f7e7e
JEONSEUNGREE <jws08219@naver.com>
2021-05-23 20:14:18 +0900

    [전승리]10일차 복습, 문제

--- java/Seungreejeon/10일차/src/ClassArrayTest.java
+++ java/Seungreejeon/10일차/src/ClassArrayTest.java
@@ -1,69 +1,75 @@
 import java.util.Scanner;
 
-class ScoresTest{
-    final  int Max = 5;
-    int randArr[];
-    int sum;
-    float mean;
-    //생성자 , randArr가 5명의 점수를 입력받는다. 50점~100점
-    public ScoresTest() {
-        sum = 0;
-        randArr = new int[Max];
+class ScoresTest {
+    final int MAX = 5;
 
-        for (int i = 0; i < Max; i++) {
+    float sum;
+    float mean;
+    int randArr[];
+
+    public ScoresTest () {
+        // 5개의 배열을 만들고 랜덤값을 할당함
+        System.out.println("생성자 호출!");
+        sum = 0;
+        randArr = new int[MAX];
+
+        for (int i = 0; i < MAX; i++) {
             randArr[i] = (int)(Math.random() * 50 + 50);
         }
     }
-    //평균
-    public void calcMean() {
-        for (int i = 0; i < Max; i++) {
+    public void calcMean () {
+        for (int i = 0; i < MAX; i++) {
             sum += randArr[i];
         }
-        mean = sum / (float)Max;
-    }
 
-    public int getSum() {
-        return sum;
-    }
-
-    public float getMean() {
-        return mean;
+        mean = sum / (float)MAX;
     }
 
     public int[] getRandArr() {
         return randArr;
     }
-
-    public int getMax() {
-        return Max;
+    public float getSum() {
+        return sum;
+    }
+    public float getMean() {
+        return mean;
+    }
+    public int getMAX() {
+        return MAX;
     }
 }
 
-
 public class ClassArrayTest {
     public static void main(String[] args) {
-        // 클래스 <==== 커스텀 데이터타입(우리가 커스텀하여 만들 수있는 데이터타입)
-        ScoresTest[] st;
-        Scanner sc = new Scanner(System.in);
+        // 클래스 <=== 커스텀 데이터타입(우리가 커스텀하여 만들 수 있는 데이터타입)
+        ScoresTest st[];
+        Scanner scan = new Scanner(System.in);
 
-        //
-        System.out.println("몇개의 학급이 있나요? :");
-        int num = sc.nextInt();
+        System.out.print("몇 개의 학급이 있나요 ? ");
 
+        int num = scan.nextInt();
+
+        // 클래스 형식의 커스텀 데이터타입으로 만들어진 배열을 num 개수만큼 만듭니다.
+        // 그리고 st라는 변수명이 이 배열 메모리 공간을 관리합니다.
         st = new ScoresTest[num];
 
         float totalSum = 0;
         float totalNumber = 0;
-        float totalMean = 0;
-        for (int i = 0; i < num; i++) {
-            st[i] = new ScoresTest();
-            st[i].calcMean();
-            float tmpSum = st[i].getSum();
 
+        // --------------------------------------
+        // | 객체1 | 객체2 | 객체3 | 객체4 | 객체5 |
+        // --------------------------------------
+        //    [0]    [1]    [2]     [3]    [4]
+
+        for (int i = 0; i < num; i++) {
+            st[i] = new ScoresTest();   // 이 부분을 통해 객체1, 객체2, ... 객체5가 생성됨
+            st[i].calcMean();
+
+            float tmpSum = st[i].getSum();
             totalSum += tmpSum;
-            totalMean += st[i].getMean();
-            totalNumber += st[i].getMax();
-            System.out.println("각반의 평균은 = "+ st[i].getMean());
+            totalNumber += st[i].getMAX();
+            System.out.println("각 객체별 합산값 = " + tmpSum);
+            System.out.println("각 반의 평균 = " + st[i].getMean());
         }
         System.out.println("최종 계산된 전체 평균은 = " + (totalSum / totalNumber));
     }
--- java/Seungreejeon/10일차/src/Quiz.java
+++ java/Seungreejeon/10일차/src/Quiz.java
@@ -2,39 +2,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Salaryman {
-    int randomMoney;
+    float randomMoney;
     float random_Rate;
-    int random_RateMoney;
-    int toTal = 0;
+    float random_RateMoney;
+    float toTal = 0;
     float arrRecord[] = new float[10];
-    int sum = 0;
+    float sum = 0;
 
     //100만원단위까지만 출력하는 방법..
+    //인상률이 소숫점 단위로 계산되서 연봉금액차이가 발생함..
+    //2년차 출력 줄만 왜 띄어써지는지 모르겠다.
+    //무엇보다 변수들이 의식의흐름대로 짜여진 코드같아서 보기가 너무힘들다...
 
+    //생성자 클래스배열용
     public Salaryman() {
         RandomBox();
-        Random_Rate();
-        System.out.printf("  1년차 연봉 : %d \n",randomMoney);
-        toTal = (int)randomMoney;
+        toTal = randomMoney;
+        System.out.printf("  1년차 연봉 : %.0f \n",randomMoney);
         arrRecord[0] = randomMoney;
+        sum +=randomMoney;
 
-       for (int i = 1; i < 10; i++) {
-           Random_Rate();
-           toTal += random_RateMoney;
-           System.out.printf( i + 1 +" 년차 인상률 : %.1fP  연봉 : %d 만원 \n  ", (random_Rate*100), toTal);
-           sum += toTal;
-           arrRecord[i] = toTal;
+        for (int i = 1; i < 10; i++) {
+            Random_Rate();
+            toTal += randomMoney*random_Rate;
+            System.out.printf( i + 1 +" 년차 인상률 : %.1fP  연봉 : %.0f 만원 \n  ", (random_Rate*100), toTal);
+
+            sum += toTal; //합계 저장
+            arrRecord[i] = toTal; // 배열에 저장
+
         }
+
     }
+
     //랜덤 초봉 2400~3500
-   public void RandomBox() {
-        randomMoney = (int) (Math.random() *1100)+ 2400;
+    public void RandomBox() {
+        randomMoney = (float)(Math.random() *1100)+ 2400;
     }
 
     //랜덤 인상율 범위 1~20%
     void Random_Rate() {
         random_Rate = ((float)(Math.random() * 19 + 1))/100;
-        random_RateMoney = (int)(randomMoney*random_Rate);
     }
 
     public float[] getArrRecord() {
@@ -62,34 +69,34 @@ public class Quiz {
         float ts_sum = 0;
         float selectSum =0;
         String[] Name = {"김씨","이씨","박씨","전씨","신씨","서씨","고씨","주씨","장씨","문씨"};
-
+        //사원 10명 평균연봉 출력
         for (int i = 0; i < 10; i++) {
-            System.out.println(Name[i]);
+            System.out.printf("  이름 : %s\n\n",Name[i]);
 
             ss[i] = new Salaryman();
+            System.out.println();
 
             sum = ss[i].getSum();
             tmpSum += sum;
             totalYear += ss[i].arrRecord.length;
             PeopleSum += tmpSum;
 
-            System.out.printf("평균 연봉 : %.0f만원 \n", tmpSum/10);
+            System.out.printf("  평균 연봉 : %.0f만원 \n\n", tmpSum/10);
             tmpSum= 0;
         }
 
         //사원 10명의 10년 평균 연봉
-        System.out.printf("사원 10년 평균 연봉 : %.0f 만원\n",PeopleSum/ totalYear);
+        System.out.printf("  사원 10년 평균 연봉 : %.0f 만원\n\n",PeopleSum/ totalYear);
 
         //년도 별 연봉조회
         Scanner sc = new Scanner(System.in);
-        System.out.println("조회하고자하 하는 년도 평균 연봉");
+        System.out.printf("  조회하고자하 하는 년도 평균 연봉 : ");
         int num = sc.nextInt();
         for (int i = 0; i < 10; i++) {
             selectSum = ss[i].arrRecord[num-1];
             ts_sum += selectSum;
         }
-        System.out.printf("%d 년 차 평균 연봉 조회 : %.0f 만원" ,num,ts_sum/10);
-
+        System.out.printf("  %d 년 차 평균 연봉 조회 : %.0f 만원" ,num,ts_sum/10);
 
     }
 }
