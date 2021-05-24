920c3355ab40af07758c0d61dfc7704243c60ac2
silenc3502 <gcccompil3r@gmail.com>
2021-05-24 16:22:24 +0900

    11일차: 43번 문제 풀이(이름 생략 버전)

---
+++ java/SanghoonLee/EleventhLecture/src/Prob43.java
@@ -0,0 +1,36 @@
+class RandomSalary {
+    final int MAX = 10;
+    final float PERCENT = 0.01f;
+    int[] yearSalary;
+
+    public RandomSalary () {
+        yearSalary = new int[MAX];
+
+        for (int i = 0; i < MAX; i++) {
+            yearSalary[i] = (int)(Math.random() * 1101 + 2400);
+        }
+    }
+    public void calcRandomSalary (int year) {
+        float randIncRate;
+
+        for (int i = 0; i < year; i++) {
+            randIncRate = (float)(Math.random() * 20 + 1);
+            // yearSalary[i] = (int)(yearSalary[i] + yearSalary[i] * randIncRate * PERCENT);
+            yearSalary[i] = (int)(yearSalary[i] * (1.0f + randIncRate * PERCENT));
+        }
+    }
+    public void printSalary () {
+        for (int i = 0; i < MAX; i++) {
+            System.out.println("연봉을 출력합니다: " + yearSalary[i]);
+        }
+    }
+}
+
+public class Prob43 {
+    public static void main(String[] args) {
+        RandomSalary rs = new RandomSalary();
+
+        rs.calcRandomSalary(10);
+        rs.printSalary();
+    }
+}
