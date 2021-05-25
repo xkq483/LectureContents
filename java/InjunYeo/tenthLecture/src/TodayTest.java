7938c51471df8800efcd93d8f8c32014ced41065
injun0607 <injun0607@naver.com>
2021-05-25 13:57:01 +0900

    [여인준]11일차 피드백 적용

--- java/InjunYeo/tenthLecture/src/TodayTest.java
+++ java/InjunYeo/tenthLecture/src/TodayTest.java
@@ -4,14 +4,21 @@ class Employee{
 
     Scanner scan;
     private String name;
-
     private int pay;
+    //상승한 연봉을 담을수 있는 배열 만들고 -> 상승한 연봉은 배열에 저장
+    int[] increasedPay;
+    //연봉상승률을 담을수 있는 배열을 만든다.
+    int[] increasedRatio;
 
     public Employee(){
         System.out.print("직원 이름을 입력해주세요 : ");
         Scanner scan = new Scanner(System.in);
         this.name= scan.nextLine();
         pay =(int)(Math.random()*1100+2400);
+    }
+    public void setYearArr(int year){
+        increasedPay = new int[year];
+        increasedRatio = new int[year];
     }
 
     public int getPay(){
@@ -21,30 +28,41 @@ class Employee{
     public String getName(){
         return name;
     }
-
-
 }
 
 class PayCalculator{
     double increase;
-    Employee em[];
+    int randRatio;
+    int year;
+
+    Employee[] em;
 
 
-    PayCalculator(Employee[] em){
+    PayCalculator(Employee[] em,int year){
         this.em = em;
+        this.year = year;
+
     }
 
-    //직원들 1년 인상률
+    //직원들 1년 인상률과 상승한 연봉,연봉상승률 배열에 담기
     private void employeesIncrease(){
         for(int i=0;i<em.length;i++){
-            increase = em[i].getPay()*(((int)(Math.random()*20+1))*0.01);
+            //생성자에서 받은 year로 연봉상승 배열생성
+            em[i].setYearArr(year);
+            //연봉상승률과 상승률을 연봉상승률 배열에 담음
+            randRatio =(int)(Math.random()*20+1);
+            em[i].increasedRatio[i] = randRatio;
+            //연봉증가량과 증가량을 증가된연봉 배열에 담음
+            increase = em[i].getPay()*(randRatio*0.01);
             em[i].setPay(em[i].getPay()+(int)increase);
-            System.out.println(em[i].getName()+"의 연봉은 "+em[i].getPay());
+            em[i].increasedPay[i] = em[i].getPay();
+            //출력란
+            System.out.printf("%s의 연봉률 상승폭은 %d%%이며 상승된 연봉은 %d 입니다.\n",em[i].getName(),em[i].increasedRatio[i],em[i].increasedPay[i]);
         }
     }
 
     //입력받은 int값만큼 연봉인상
-    public void showIncrease(int year){
+    public void showIncrease(){
         for(int i =0;i<year;i++){
             System.out.println(i+"년차 연봉 평균은"+averagePay());
             employeesIncrease();
@@ -95,8 +113,8 @@ public class TodayTest {
             em[i] = e;
             System.out.println(em[i].getName()+"님의 연봉은 "+em[i].getPay());
         }
-        PayCalculator p = new PayCalculator(em);
-        p.showIncrease(3);
+        PayCalculator p = new PayCalculator(em,3);
+        p.showIncrease();
 
 
 
