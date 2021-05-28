0d39ba58ccfe126ec1fef82a5a9054c480d2b413
yoo-jong-hyeon <gowert612@gmail.com>
2021-05-28 15:26:02 +0900

    [유종현] Quiz48

--- java/JonghyeonYoo/FourteenthLecture/src/Quiz48ArrayList.java
+++ java/JonghyeonYoo/FourteenthLecture/src/Quiz48ArrayList.java
@@ -9,30 +9,31 @@ class DrawGame {
 //당첨된 사람의 이름을 출력하고 "당첨되셨습니다. 누구 누구 누구 축하합니다 ^^" 를 출력하자!
 
     ArrayList<String> nameList;
-    String[] tmpArr;
-    int[] tmpIdx;
-    int[] success;
+    String[] tmpArr;  //String 타입의 배열
+    int[] tmpIdx;     //int타입의 배열
+    int[] success;    //int타입 배열 당첨자
 
-    int nameLength;
-    Boolean isRedundant;
+    int nameLength;   // 이름의 길이를 받을 변수
+    Boolean isRedundant; // 참/ 거짓으로 중복숫자를 없애기 위함
 
     public DrawGame(String[] names) {
-        nameLength = names.length;
+        nameLength = names.length;  // nameLength안 에다가 names의 길이를 대입 (28)
         isRedundant = true;
 
         nameList = new ArrayList<String>();
-        tmpArr = new String[nameLength];
-        tmpIdx = new int[nameLength];
+        tmpArr = new String[nameLength]; // String타입 배열 인덱스 28개 생성
+        tmpIdx = new int[nameLength];   // int 타입 배열 인덱스 28개 생성
 
-        success = new int[3];
+        success = new int[3]; // 당첨자가 들어갈 인덱스 3개 생성
 
-        int i = 0;
-        for (String name : names) {
-            tmpArr[i++] = name;
+        int i = 0;  //아래 tmp[i++]의 인덱스 0부터 넣기위함.
+        for (String name : names) { //main의 names에 있는 이름을 String name에 입력한다.
+            tmpArr[i++] = name; // String name에 들어간 이름을 배열에 0번 인덱스부터 순서대로 대입
         }
     }
 
-    private Boolean checkDuplicate(int idx) {   //중복제거
+    private Boolean checkDuplicate(int idx) {   //중복제거 메소드
+                                                // 아래의 shuffle, checkSuccess 메소드의 중복을 제거
         for (int i = 0; i < idx; i++) {
             if (tmpIdx[i] == tmpIdx[idx]) {
                 return true;
@@ -50,13 +51,13 @@ class DrawGame {
         do {
             tmpIdx[i] = (int) (Math.random() * nameLength);
 
-            if (checkDuplicate(i)) {
+            if (checkDuplicate(i)) { // 위 checkDuplicate 메소드에서 tmpIdx[i] 의 중복검사 실행
                 continue;
             }
 
-            i++;
+            i++; // tmpIdx[i] 0번인덱스부터 점차 증가 (0 -> 1 -> 2 -> 3 -> ... 27)
 
-            if (i == nameLength) {
+            if (i == nameLength) {  // nameLength의 길이(28) 까지오면 탈출 0번 ~27번 인덱스까지만 실행
                 isRedundant = false;
             }
         } while (isRedundant);
@@ -69,13 +70,13 @@ class DrawGame {
         do {
             success[i] = (int)(Math.random() * nameLength);
 
-            if (checkDuplicate(i)) {
+            if (checkDuplicate(i)) { // 위 checkDuplicate 메소드에서 success[i]의 중복검사를 실행
                 continue;
             }
 
-            i++;
+            i++; // 0번부터 점차 증가 +1  (0 -> 1 -> 2 ....)
 
-            if (i == 3) {
+            if (i == 3) {  //인덱스 3번방까지오면 탈출한다?? 0번 ~ 2번 인덱스까지 실행
                 isRedundant = false;
             }
         } while (isRedundant);
