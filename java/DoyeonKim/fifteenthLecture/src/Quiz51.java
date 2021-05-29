9e10a506e58be97abd97cd9678a2e8eb3c29bbf5
ms109420 <ms109420@naver.com>
2021-05-29 22:05:16 +0900

    [김도연] 15일차 복습

---
+++ java/DoyeonKim/fifteenthLecture/src/Quiz51.java
@@ -0,0 +1,22 @@
+class ShopList {
+
+    public void buylist () {
+        String[] buylist = {"스위치", "애플워치", "책", "버즈"};
+       
+    }
+}
+
+public class Quiz51 {
+    public static void main(String[] args) {
+        ShopList buy = new ShopList();
+        buy.buylist();
+
+
+    }
+}
+
+// 리스트로 목록을 만든다
+// 구매 판매 리스트 만들기
+// 구매할 물건을 적으면 목록 인덱스에서 그 목록이 지워지도록 만들기
+//그리고 판매리스트에 넣는다
+//소지금은 내가 정해줘야하는건가? 입력받도록 해야하나
