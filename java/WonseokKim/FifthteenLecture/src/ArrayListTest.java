478047e94c226a69b46f7a140d3cff80137e9dcb
wsk0307 <wsk0307@naver.com>
2021-05-31 15:06:39 +0900

    [15일차] 수업내용 복습 및 숙제

---
+++ java/WonseokKim/FifthteenLecture/src/ArrayListTest.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "strawberry", "grape", "watermelon"};
+        // asList()를 통해 배열등을 ArrayList로 변형할 수 있음
+        ArrayList<String> fruitsList = new ArrayList<>(Arrays.asList(fruits));
+
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("get(%d) = %s\n", i, fruitsList.get(i));
+        }
+
+        System.out.println("복제 이후!");
+
+        // fruitsList의 내용을 clone 변수에 복제함
+        // ArrayList에 구현되어 있는 clone은 객체를 복제할 수 있게 서포트한다.
+        ArrayList<String> clone = (ArrayList<String>) fruitsList.clone();
+
+        for (int i = 0; i < 4; i++) {
+            // ArrayList에 있는 내용을 가져올때 get(index)를 사용합니다.
+            // 여기서 index는 | 데이터1 | ---> | 데이터2 | ---> | 데이터8 | --->
+            //                   0               1               2
+            System.out.printf("get(%d) = %s\n", i, clone.get(i));
+        }
+
+        fruitsList.remove("grape");
+        System.out.println("fruitsList의 grape 삭제 이후 clone 다시 보기");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("get(%d) = %s\n", i, clone.get(i));
+        }
+
+        System.out.println("fruitsList는 지워졌을까 ?");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("get(%d) = %s\n", i, fruitsList.get(i));
+        }
+
+        // 완전 통채로 밀림
+        fruitsList.clear();
+
+        System.out.println("after clear: " + fruitsList);
+
+        // 검색 - indexOf
+        System.out.println("포도 어딧니 ? " + clone.indexOf("grape"));
+        System.out.println("딸기는 ? " + clone.indexOf("strawberry"));
+        // 존재하지 않는 것은 -1 이 나옴(오류)
+        System.out.println("드립을 치고 싶은데 ? " + clone.indexOf("드립"));
+
+        // 당첨자 명단 ~~~~
+        // 공모자 명단 ~~~~
+        // 시상식 참석자 명단 ~~~
+        // for (참석자 명단 ~~~) {
+        //     if (indexOf("당첨자 명단") == -1) {
+        //         continue;
+        //     } else {
+        //         cnt++
+        //     }
+        // }
+        // 위와 같은 형태로 시상식에 참석하면 수상자 숫자를 집계할 수 있다.
+
+        // contains 또한 IndexOf와 유사하게 사용이 가능하다.
+        System.out.println("포도 어딧니 ? " + clone.contains("grape"));
+        System.out.println("딸기는 ? " + clone.contains("strawberry"));
+        System.out.println("드립을 치고 싶은데 ? " + clone.contains("드립"));
+
+        // contains()는 참 혹은 거짓으로 결과가 나오는 것을 확인할 수 있다.
+    }
+}
--- java/WonseokKim/MyStudyJava/src/FirstLecture.java
+++ java/WonseokKim/MyStudyJava/src/FirstLecture.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class FirstLecture {
     public static void main(String[] args) {
+        // 변수 이론
 
+        // 변수란 값을 기록하기 위한 메모리(RAM) 공간.
+        // 자료형(TYPE)에는
+        // 각각 boolean, char, String(참조형), byte, short, int, long, float, double 이렇게 구분되어져 있다.
+        // *** 주목해야할 점 ***
+        // long 타입은 값에 L를 붙여준다 ex) 0L
+        // float 타입은 값에 f를 붙여준다 ex)0f
+        // double 타입은 실수의 기본형이기 때문에 대부분 따로 붙이진 않지만 구분을 위해서 0.0d라고 붙여줄수도 있다.
 
 
 
@@ -22,5 +32,21 @@ public class FirstLecture {
         bool = false;
 
         System.out.println("값 변경 후 : " + inum + "," + dnum + "," + ch +"," + str + ","+ bool);
+
+
+        // 실습 2번 문제
+        // 키보드로 원의 반지름 값을 정수형으로 입력 받고 원의 둘레와 면적을 계산하여 소수점 이하 1자리까지만 출력하는 코드를 작성하시오.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("원의 반지름 입력: ");
+        int r = scan.nextInt();
+        float pi = 3.14f;
+
+        float circle = r * 2 * pi;
+        float area = r * r * pi;
+        System.out.printf("원의 둘레의 값은 %.2f\n", circle);
+        System.out.println("원의 면적의 값은 = " + area);
+
+
     }
 }
