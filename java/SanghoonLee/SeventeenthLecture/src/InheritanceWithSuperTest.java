4a97446326287c3357df9b4c79151c028dc932fe
silenc3502 <gcccompil3r@gmail.com>
2021-06-01 19:39:20 +0900

    17일차: 52번 문제 풀이(한글 클래스 가능 - 되도록이면 자제하자)

--- java/SanghoonLee/SeventeenthLecture/src/InheritanceWithSuperTest.java
+++ java/SanghoonLee/SeventeenthLecture/src/InheritanceWithSuperTest.java
@@ -10,6 +10,16 @@ class Vehicle {
         this.pressure = pressure;
         this.color = color;
     }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "rpm=" + rpm +
+                ", fuel=" + fuel +
+                ", pressure=" + pressure +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
 
 class Airplane extends Vehicle {
@@ -19,16 +29,37 @@ class Airplane extends Vehicle {
 
     public Airplane(float rpm, float fuel, float pressure, String color,
                     float aileron, float pitch, float rudder) {
+        // super()는 무엇이 되었든 상속자인 부모를 호출한다.
+        // super()만 적혀 있으니 생성자를 호출하게 된다.
         super(rpm, fuel, pressure, color);
 
         this.aileron = aileron;
         this.pitch = pitch;
         this.rudder = rudder;
     }
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                // super.toString()은 부모 클래스의 toString()을 호출한 것이다.
+                "super.Vehicle()=" + super.toString() +
+                ", aileron=" + aileron +
+                ", pitch=" + pitch +
+                ", rudder=" + rudder +
+                '}';
+    }
 }
 
 public class InheritanceWithSuperTest {
     public static void main(String[] args) {
+        Vehicle v = new Vehicle(200, 1.2f, 1.0f, "Red");
 
+        System.out.println(v);
+
+        Airplane a = new Airplane(
+                1000, 112.5f, 12.3f, "White",
+                77.3f, 0.02f, 33.9f);
+
+        System.out.println(a);
     }
 }
