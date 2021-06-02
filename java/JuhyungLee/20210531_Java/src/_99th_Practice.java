77f0d8d7e4323d98761a9a95ffda4ec496049853
spmen12 <spmen12@naver.com>
2021-06-02 14:12:42 +0900

    20210601 수업자료

--- java/JuhyungLee/20210531_Java/src/_99th_Practice.java
+++ java/JuhyungLee/20210531_Java/src/_99th_Practice.java
@@ -2,22 +2,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class _99th_Practice {
     public static void main(String[] args) {
-//        int[] array_numbers = { 1, 2, 3, 4, 5, 6, 7};
+        int[] array_numbers = {1, 2, 3, 4, 5, 6, 7};
 //        System.out.println(array_numbers);
 //        for(int i = 0; i < array_numbers.length; i++){
 //            System.out.print(array_numbers[i]);
 //        }
 //        System.out.println();
+        ArrayList<int[]> arNum = new ArrayList<int[]>(Arrays.asList(array_numbers));
+        System.out.println(arNum);
 
-//        ArrayList<Integer> arrayList_numbers = new ArrayList<Integer>(Arrays.asList(array_numbers));
+//        ArrayList<Integer> arNum2 = new ArrayList<Integer>(Arrays.asList(array_numbers));
+//        System.out.println(arNum);
 
-        String[] fruits = {"apple", "strawberry", "melon"};
-        ArrayList<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
-
-        System.out.println(fruits_list);
-
-        for(String f : fruits){
-            System.out.print(f + " ");
-        }
+//        String[] fruits = {"apple", "strawberry", "melon"};
+//        ArrayList<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+//
+//        System.out.println(fruits_list);
+//
+//        for (String f : fruits) {
+//            System.out.print(f + " ");
+//        }
     }
 }
