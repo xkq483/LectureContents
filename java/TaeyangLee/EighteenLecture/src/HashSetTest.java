7a2558b4792dfbda8af1a257ee7ba68fc4e3e80e
rudghxodid <rudghxodid@naver.com>
2021-06-02 23:50:34 +0900

    [이태양]6월2일 복습(해시셋,인터페이스 등)

---
+++ java/TaeyangLee/EighteenLecture/src/HashSetTest.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class HashSetTest {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<String>();
+
+        set.add("우유");
+        set.add("빵");
+        set.add("베이컨");
+        set.add("소시지");
+        set.add("파스타");
+    }
+}
