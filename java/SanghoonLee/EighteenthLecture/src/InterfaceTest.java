6629388d2248b7100bbfd1fae1303db3bf547e9a
silenc3502 <gcccompil3r@gmail.com>
2021-06-02 17:43:21 +0900

    18일차: implements를 사용해서 중간 매개체를 지워버림

---
+++ java/SanghoonLee/EighteenthLecture/src/InterfaceTest.java
@@ -0,0 +1,59 @@
+interface LampMethod {
+    public void lightOn();
+    public void lightOff();
+}
+
+class Lamp implements LampMethod {
+    @Override
+    public void lightOn() {
+        System.out.println("Lamp를 킵니다.");
+    }
+
+    @Override
+    public void lightOff() {
+        System.out.println("Lamp를 끕니다.");
+    }
+}
+
+class StreetLamp implements LampMethod {
+    @Override
+    public void lightOn() {
+        System.out.println("가로등을 킵니다.");
+    }
+
+    @Override
+    public void lightOff() {
+        System.out.println("가로등을 끕니다.");
+    }
+}
+
+class Led implements LampMethod {
+    @Override
+    public void lightOn() {
+        System.out.println("LED등을 킵니다.");
+    }
+
+    @Override
+    public void lightOff() {
+        System.out.println("LED등을 끕니다.");
+    }
+}
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        Lamp lamp = new Lamp();
+
+        lamp.lightOn();
+        lamp.lightOff();
+
+        StreetLamp streetLamp = new StreetLamp();
+
+        streetLamp.lightOn();
+        streetLamp.lightOff();
+
+        Led led = new Led();
+
+        led.lightOn();
+        led.lightOff();
+    }
+}
