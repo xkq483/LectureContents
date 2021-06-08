925c989bdb2859a7d2a9e67d0662565e41675b42
JEONSEUNGREE <jws08219@naver.com>
2021-06-08 12:40:36 +0900

    [전승리]21일차 복습

--- java/Seungreejeon/21Days/src/Prob60.java
+++ java/Seungreejeon/21Days/src/Prob60.java
@@ -11,39 +11,36 @@ class FrequencyChecker {
         frequencySet = new HashSet<Integer>();
         frequancyMap = new HashMap < Integer, Integer > ();
 
-        backup = arr;
+        backup = arr; //입력받은 배열 백업배열에 저장
 
-        for (Integer elem : arr) {
+        for (Integer elem : arr) { //입력받은 배열 set과 map의 key값에 추가
             frequencySet.add(elem);
             frequancyMap.put(elem, 0);
 
         }
     }
 
-    public Map<Integer, Integer> getFrequancyMap() {
+    public Map<Integer, Integer> getFrequancyMap() { //중복된 수 확인하기위한 getter
         return frequancyMap;
     }
 
     public void allocRandomFrequency(int num) {
         for (int i = 0; i < num; i++) {
             int tmp = (int) (Math.random() * 10);
-            int key = backup[tmp];
+            int key = backup[tmp]; //랜덤생성번호에 위치한 배열정보를 key 값에 대입
 
             System.out.printf("%6d",key);
 
-            if (i % 5 == 4) {
+            if (i % 5 == 4) { //줄 바꿈
                 System.out.println();
             }
 
-            if (frequencySet.contains(key)) {
-
-                int cnt = frequancyMap.get(key);//해당 key 해당하는 value 값을 가져온다는 것이다 혼동 주의
+            if (frequencySet.contains(key)) { //키값이 포함되어있으면 true
+                int cnt = frequancyMap.get(key);//해당 key 해당하는 value 값을 가져온다는 것이다
                 frequancyMap.put(key, ++cnt); // 값 증가
             }
         }
-
     }
-
 }
 
 public class Prob60 {
