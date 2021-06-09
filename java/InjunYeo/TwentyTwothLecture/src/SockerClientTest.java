66ea33df0d09eb02db527d092d5e96cfb404f8ea
injun0607 <injun0607@naver.com>
2021-06-09 15:29:43 +0900

    [여인준]22일차 복습

---
+++ java/InjunYeo/TwentyTwothLecture/src/SockerClientTest.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+
+public class SockerClientTest {
+    public static void main(String[] args) {
+
+        //사설망이라 컴퓨터 털릴일 없으니 걱정 no!
+        String hostname = "192.168.0.13";
+        int port = 33333;
+
+        for(int i=0;i<10;i++){
+            try{
+                Socket sock = new Socket(hostname,port);
+                OutputStream out = sock.getOutputStream();
+
+                String str = "Hello Network Programming!!!";
+
+                out.write(str.getBytes());
+
+                InputStream in = sock.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String time = reader.readLine();
+                System.out.println(time);
+            } catch (UnknownHostException e){
+                System.out.println("Server Not Found: "+e.getMessage());
+            } catch (IOException e ){
+                System.out.println("I/O Error: "+e.getMessage());
+            }
+
+        }
+
+    }
+}
