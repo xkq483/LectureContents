47c6f15e40a7687b02b15ecd12efa34f915e4dae
Sanghoon Lee <gcccompil3r@gmail.com>
2021-06-09 20:37:43 +0900

    [26일차]김창욱 숙제입니다.

--- java/ChangwookKim/ThirdLecture/src/IfTestWithinput.java
+++ java/ChangwookKim/ThirdLecture/src/IfTestWithinput.java
@@ -2,20 +2,28 @@ import java.util.Scanner;
 
 public class IfTestWithinput {
     public static void main(String[] args) {
-        System.out.println("두 개 숫자를 입력 받아 비교해봅니다.");
+        // 아무런 숫자나 입력 받아 3의 배수인지 판별을 해봅시다.
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = scan.nextInt();
+        System.out.print("아무 숫자나 입력해보세요(11의 배수를 판정하겠습니다): ");
+        int num = scan.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = scan.nextInt();
-
-        if (num1 > num2) {
-            System.out.printf("%d > %d\n", num1, num2);
+        if (num % 11 == 0) {
+            System.out.println(num + " 은 11의 배수입니다.");
         } else {
-            System.out.printf("%d < %d\n", num1, num2);
+            System.out.println(num + " 은 11의 배수가 아닙니다.");
+        }
 
+        // 1 ~ 키보드로 입력 받은 숫자중 3의 배수를 판별하여 출력
+        System.out.print("1 ~ n까지 숫자중 3의 배수를 찾아봅시다(n을 입력하세요): ");
+        int loop = scan.nextInt();
+        int i = 1;
+
+        while (i <= loop) {
+            if (i % 3 == 0) {
+                System.out.println("1 ~ " + loop + " 까지 3의 배수를 출력합니다: " + i);
+            }
+            i++;
         }
     }
 }
