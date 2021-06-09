165b261f7b79054c268e437d7f5ca0ee0c91e78f
jjrk1231 <jjrk1231@naver.com>
2021-06-09 20:48:29 +0900

    [문성호] 0608

---
+++ java/SunghoMoon/java 0607/src/client.java
@@ -0,0 +1,14 @@
+
+
+public  class client {
+    public void test() {}
+    public void test(String str) {}
+    public void test(int i, String str) {}
+    public void test(char ch) {}
+    public int test() {return 0;}
+    public void test(int i) {}
+    public void test(String s) {}
+    public void test(String str, int i) {}
+    private void test (int i) {}
+    }
+}
