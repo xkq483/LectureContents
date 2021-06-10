bf39307064b129286593ae6420adddd7ab3b4070
wsk0307 <wsk0307@naver.com>
2021-06-10 15:00:23 +0900

    23일차 수업 복습

--- java/WonseokKim/MyStudyJava/src/WholeReviewFirst.java
+++ java/WonseokKim/MyStudyJava/src/WholeReviewFirst.java
@@ -3,6 +3,147 @@ import java.util.Scanner;
 
 public class WholeReviewFirst {
     public static void main(String[] args) {
+
+        //=====종합 실습 문제=====//
+
+        //빙고 게임
+        // 사용자로부터 빙고판의 크기를 입력 받아 지정 된 수의 가로 * 세로 빙고판에
+        // 1부터 가로 * 세로의 값까지의 수를 랜덤하게 발생시켜 빙고판을 만드시오.
+        // 다만 중복되는 수는 없어야 하며, 숫자를 선택하면 해당 칸이 0으로 바뀌고
+        // 모든 칸의 값이 0이 되면 프로그램을 종료하시오.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("일차원 배열의 길이: ");
+        int len = sc.nextInt();
+
+        //일차원 배열로 중복 값 제거한 후 이차원 배열 대입
+        int[] tempArr = new int[len * len];
+
+        //랜덤 값 대입 후 중복 비교
+        for (int i = 0; i < tempArr.length; i++) {
+            tempArr[i] = (int) (Math.random() * (len * len) + 1);
+            // 중복 값 발생하면 i--
+            // 즉 i+1로 진행하지 않고 다시 i에 랜덤 값 대입 후 중복 비
+            for (int j = 0; j < i; j++) {
+                if (tempArr[i] == tempArr[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+        // 중복 값 제거가 잘 되어있는지 일차원 배열 출력
+        System.out.println(Arrays.toString(tempArr));
+        // 일차원 배열 -> 이차원 배열
+        int[][] arr = new int[len][len];
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                arr[i][j] = tempArr[index++];
+            }
+        }
+        System.out.println("==========빙고 게임 시작===========");
+        //빙고판 출력
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        //모든 자리가 0이 될때까지 무한 반복
+        while (true) {
+            System.out.print("숫자 선택: ");
+            int num = sc.nextInt();
+            //선택한 숫자를 0으로 바꾼 뒤 출력
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == num) {
+                        arr[i][j] = 0;
+                    }
+                    System.out.print(arr[i][j] + "\t");
+                }
+                System.out.println();
+            }
+
+           /* //게임 종료 조건확인)모든 자리가 0이 된다
+            boolean flag = false;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if(arr[i][j] != 0){
+                      flag =true;
+                    }
+                  }
+                }
+                //모든 칸이 0이면 게임종 료
+                if(flag == false) {
+
+            }*/
+
+            //3빙고이면 게임 종료
+
+            int bingoCnt = 0;
+            int crossCnt1 = 0;
+            int crossCnt2 = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                int colCnt = 0;
+                int rowCnt = 0;
+                for (int j = 0; j < arr[i].length; j++) {
+                    //가로
+                    if (arr[i][j] == 0) {
+                        rowCnt++;
+                    }
+                    //세로
+                    if (arr[j][i] == 0) {
+                        colCnt++;
+                    }
+                    //왼쪽에서 오른쪽 아래로 대각선
+                    if (i == j && arr[i][j] == 0) {
+                        crossCnt1++;
+                    }
+                    //오른쪽에서 왼쪽 아래로 대각선
+                    if ((i + j == len - 1) && arr[i][j] == 0) {
+                        crossCnt2++;
+                    }
+                    //가로 한줄이 모두 0이면 빙고
+                    if (rowCnt == len) {
+                        bingoCnt++;
+                    }
+                    //세로한줄이 모두 0이면 빙고
+                    if (colCnt == len) {
+                        bingoCnt++;
+                    }
+                    //왼쪽에서 오른쪽 아래로 대각선이 모두 0이면 빙고
+                    if (crossCnt1 == len) {
+                        bingoCnt++;
+                    }
+                    //오른쪽에서 왼쪽 아래로 대각선이 모두 0이면 빙
+                    if (crossCnt2 == len) {
+                        bingoCnt++;
+                    }
+                    //빙고가 3개이면 게임 종료
+                    if (bingoCnt == 3) {
+                        break;
+                    }
+                }
+            }
+            System.out.println("빙고 완성!!!");
+            System.out.println("========빙고 게임 종료=========");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
         //char <-> int 형변환에 대해 연습
         int num = 'A';
         System.out.println("num: " + num); //65
@@ -135,7 +276,7 @@ public class WholeReviewFirst {
         int[] copyArr2 = new int[10];
         for (int i = 0; i < originArr.length; i++) {
             copyArr2[i] = originArr[i];
-            System.out.println(copyArr2[i]);
+            System.ou t.println(copyArr2[i]);
         }
         //깊은 복사 arraycopy()
         int[] copyArr3 = new int[10];
@@ -147,7 +288,7 @@ public class WholeReviewFirst {
 
         System.out.println(copyArr4);
 
- */
+
         //2차원 배열 (가변 배열)
         int[][] arr = new int[2][];
         arr[0] = new int[4];
@@ -167,9 +308,9 @@ public class WholeReviewFirst {
             System.out.println();
         }
 
+ */
 
 
-
-
+            }
+        }
     }
-}
