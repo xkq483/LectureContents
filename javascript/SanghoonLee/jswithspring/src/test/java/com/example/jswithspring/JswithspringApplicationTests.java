fffde30f7c3c839dfa2923817a853c08e036e31c
silenc3502 <gcccompil3r@gmail.com>
2021-06-10 20:06:23 +0900

    24일차: Spring Controller 작성 준비 완료!

---
+++ javascript/SanghoonLee/jswithspring/src/test/java/com/example/jswithspring/JswithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jswithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JswithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
