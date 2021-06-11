1399e620dd674e479520cd5eef933c8fff3b02c1
JEONSEUNGREE <jws08219@naver.com>
2021-06-11 22:01:06 +0900

    [전승리]25일차_수업

--- javascript/Seungreejeon/demo/src/main/java/com/example/demo/controller/HtmlTestController.java
+++ javascript/Seungreejeon/demo/src/main/java/com/example/demo/controller/HtmlTestController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.utility.Test;
+import com.example.demo.utility.network.Client;
+import com.example.demo.utility.network.Server;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +15,10 @@ public class HtmlTestController {
     // HtmlTestController가 제어하는 요소를 디버깅하도록 서포트하는 객체
     // static final Logger log = LoggerFactory.getLogger(HtmlTestController.class);
 
-    @GetMapping("/")
+    //실제 웹상에서 URL 요청할 때는 자주 사용하는 두 가지 방식이있다.
+    //Get, Post 방식인데 우선 일반적인 URL 입력은 Get으로 파악하면 되겠다.
+    // 현재 우리의 ip의
+    @GetMapping("/") //메인 페이지를 맵핑한다.
     public String firstIndex () {
         // 위에 Logger 사용하는 방식은 Lombok이 없을 경우 사용함
         // 그래서 이클립스에서는 상당히 불편하게 구성해야하는데 비해
@@ -21,5 +27,43 @@ public class HtmlTestController {
         log.info("firstIndex() 실행");
 
         return "htmlHi";
+    }
+
+    @GetMapping("/comparatorTest")
+    public String comparatorTest () {
+        log.info("comparatorTest()");
+
+        Test test = new Test();
+        test.doTest();
+
+        return "test";
+    }
+    @GetMapping("/fail")
+    public String fail () {
+        log.info("fail()");
+
+        return "fail";
+    }
+
+
+    @GetMapping("/serverOn")
+    public String startServer() {
+
+        log.info("startServer()");
+
+        Server server = new Server();
+        server.waitForClientTest();
+
+        return "test";
+    }
+
+    @GetMapping("/clientOn")
+    public String startClient () {
+        log.info("startClient()");
+
+        Client client = new Client();
+        client.connectToServerTest();
+
+        return "test";
     }
 }
