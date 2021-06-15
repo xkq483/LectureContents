88d49981023e64bd1d3dc4667b279c8d9a89958f
jjrk1231 <jjrk1231@naver.com>
2021-06-15 14:40:51 +0900

    [문성호] 26일차 복습

---
+++ javascript/SunghoMoon/demo/src/main/java/com/example/demo/controller/SecondController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+
+public class SecondController {
+
+    @GetMapping("/heightWidthTest")
+    public String heightWidthTest() {
+        log.info("heightWidthTest()");
+
+        return "/second/heightWidthTest";
+
+    }
+
+    @GetMapping("/divTest1")
+    public String divTest1() {
+        log.info("deiTest1()");
+
+        return "/second/divTest1";
+    }
+
+    @GetMapping("/divTest2")
+    public String divTest2() {
+        log.info("deiTest2()");
+
+        return "/second/divTest2";
+    }
+
+    @GetMapping("/divTest3")
+    public String divTest3() {
+        log.info("deiTest3()");
+
+        return "/second/divTest3";
+
+    }
+
+    @GetMapping("/aTagTest")
+    public String aTagTest() {
+        log.info("aTagTest()");
+
+        return "/second/aTagTest";
+
+    }
+
+    @GetMapping("/formTest1")
+    public String formTest1() {
+        log.info("formTest1()");
+
+        return "/second/formTest1";
+    }
+    @PostMapping("/memberRegister")
+    public String memberRegister() {
+        log.info("memberRegister()");
+
+        return "redirect:/";
+    }
+}
