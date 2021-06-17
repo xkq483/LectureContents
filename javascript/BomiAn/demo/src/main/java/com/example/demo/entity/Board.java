c60cb05d8b6c08ac55866257fea726c1a5c34fec
BomiAn <qhal4686@gmail.com>
2021-06-17 21:39:31 +0900

    [안보미] - 29일차 수업내용 정리 (게시판 만들기) DB연동

---
+++ javascript/BomiAn/demo/src/main/java/com/example/demo/entity/Board.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@ToString
+@Getter
+@Setter
+public class Board {
+
+    private int BoardNO;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regDate;
+}
---
+++ javascript/BomiAn/demo/src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.Board;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BoardService {
+    public void register(Board board) throws Exception;
+
+}
