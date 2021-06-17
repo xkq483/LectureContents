70946af3d8e291aa5b37179a875bff7281db4fd2
kmg13579 <980steroids@naver.com>
2021-06-18 00:31:33 +0900

    [김민규] 29일 수업내용 복습

---
+++ javascript/MinkyuKim/demo/src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Board;
+import com.example.demo.repository.BoardRepository;
+import com.example.demo.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// Service는 여기서 register가 여러 방식으로 동작할 수 있음을 명시한다.
+// 또한 Controller의 Autowired에 자동으로 연결되도록 서포트한다.
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @Override
+    public void register(Board board) throws Exception {
+        repository.create(board);
+    }
+}
