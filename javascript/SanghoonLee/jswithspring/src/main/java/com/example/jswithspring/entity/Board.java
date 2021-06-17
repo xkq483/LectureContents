1ea859b84dd22465bdb013fcd9434afed39b2521
silenc3502 <gcccompil3r@gmail.com>
2021-06-17 19:51:56 +0900

    29일차: 게시글 등록(toString 처리 + addAttribute 연동)

--- javascript/SanghoonLee/jswithspring/src/main/java/com/example/jswithspring/entity/Board.java
+++ javascript/SanghoonLee/jswithspring/src/main/java/com/example/jswithspring/entity/Board.java
@@ -2,11 +2,13 @@ package com.example.jswithspring.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@ToString
 public class Board {
     private int boardNo;
     // th:field로 맵핑됨
