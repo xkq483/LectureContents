051ea7098e62498427b212d37aee58c1ab2fd826
bombom23 <tkddn5678@naver.com>
2021-06-18 14:39:32 +0900

    [한상우]6월17일복습및 질문노트

---
+++ javascript/SangwooHan/demo/src/main/java/com/example/demo/entity/NagetloningTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+@Getter
+@Setter
+@ToString
+public class NagetloningTest {
+    private int boardNo;
+    private String id;
+    private String address;
+    private String pw;
+    private Date regDate;
+}
---
+++ javascript/SangwooHan/demo/src/main/java/com/example/demo/repository/NagNetReopsitory.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.NagetloningTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NagNetReopsitory {
+    @Autowired
+    private JdbcTemplate jdbcTemplate2;
+
+    public void creates(NagetloningTest nagetloningtest) throws Exception{
+        String query = "insert into NagNetTest (id, address, pw) values (?,?,?)";
+        jdbcTemplate2.update(query,nagetloningtest.getId(), nagetloningtest.getAddress(), nagetloningtest.getPw());
+    }
+}
