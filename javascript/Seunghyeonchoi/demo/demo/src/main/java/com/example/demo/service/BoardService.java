4e3a829acb11e116bd42d16122eb9b3beae65ea0
KH-CSH <endras132@naver.com>
2021-06-18 14:56:41 +0900

    29일차 내용 복습

---
+++ javascript/Seunghyeonchoi/demo/demo/src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Board;
+import org.springframework.stereotype.Service;
+public interface BoardService {
+    public void register(Board board) throws Exception;
+}
