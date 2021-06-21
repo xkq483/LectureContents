7cc1788c3f8c82114daac4d224f246ae4e2810c5
yoo-jong-hyeon <gowert612@gmail.com>
2021-06-21 15:42:27 +0900

    [유종현] 30일차 수업복습(게시판 등록&목록보기)

--- javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/entity/Board.java
+++ javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/entity/Board.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
-
+//
 @Getter
 @Setter
 @ToString
--- javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/service/BoardService.java
+++ javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/service/BoardService.java
@@ -3,6 +3,10 @@ package com.example.jsmain.service;
 import com.example.jsmain.entity.Board;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface BoardService {
     public void register(Board board) throws Exception;
+
+    public List<Board> list() throws Exception;
 }
