2707edc949de036fd67febc3559e4419fce081bb
jaqkqh15 <jaqkqh15@naver.com>
2021-06-21 12:22:07 +0900

    [황정아] (210618) 30일차 복습

--- javascript/JungahHwang/demo/src/main/java/com/example/demo/service/BoardService.java
+++ javascript/JungahHwang/demo/src/main/java/com/example/demo/service/BoardService.java
@@ -2,6 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Board;
 
+import java.util.List;
+
 public interface BoardService {
     public void register(Board board) throws Exception;
+
+    // DB안의 Board에 있는 list 정보를 가지고 온다.
+    public List<Board> list() throws Exception;
 }
--- javascript/JungahHwang/demo/src/main/java/com/example/demo/service/BoardServiceImpl.java
+++ javascript/JungahHwang/demo/src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 // Service: 여기서 register가 여러 방식으로 동작할 수 있음을 명시
 //          또한 Controller의 Autowired에 자동으로 연결되도록 서포트
 @Service
@@ -16,6 +18,10 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public void register(Board board) throws Exception{
         repository.create(board);
+    }
+    @Override
+    public List<Board> list() throws Exception {
+        return repository.list();
     }
 }
 
