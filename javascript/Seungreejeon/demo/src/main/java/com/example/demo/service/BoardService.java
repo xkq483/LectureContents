1022ecdeb07c99c9e9c598ca29924fd0e82f5612
JEONSEUNGREE <jws08219@naver.com>
2021-06-22 22:13:55 +0900

    [전승리]32일차_Read,Remove

--- javascript/Seungreejeon/demo/src/main/java/com/example/demo/service/BoardService.java
+++ javascript/Seungreejeon/demo/src/main/java/com/example/demo/service/BoardService.java
@@ -8,5 +8,9 @@ public interface BoardService {
     public void register(Board board) throws Exception;
 
     public List<Board> list() throws Exception;
+
+    public Board read(Integer boardNo) throws Exception;
+
+    public void remove(Integer boardNo) throws Exception;
 }
 
