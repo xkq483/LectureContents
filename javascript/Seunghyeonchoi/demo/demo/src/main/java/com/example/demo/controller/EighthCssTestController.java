b5086d33870e4c947d3d5090f122ee557261cd0e
KH-CSH <endras132@naver.com>
2021-06-23 18:22:40 +0900

    32일차 내용 이동4

--- javascript/Seunghyeonchoi/demo/demo/src/main/java/com/example/demo/controller/EighthCssTestController.java
+++ javascript/Seunghyeonchoi/demo/demo/src/main/java/com/example/demo/controller/EighthCssTestController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-// 해당 컨트롤러 사용할 때 무조건 URL 위치가 기본적으로 /css가 들어감
+// 해당 컨트롤러를 사용할 때 무조건 URL 위치가 기본적으로 /css가 들어감
 @RequestMapping(value = "/css")
 public class EighthCssTestController {
 
-
+    // 결국 얘는 /css/basicTest 와 같음
     @GetMapping("/basicTest")
     public String getBasicCssTest () {
         log.info("getBasicCssTest()");
@@ -26,35 +26,29 @@ public class EighthCssTestController {
         return "csstest/eighth/styleTest";
     }
 
-    @GetMapping("/marginTest")
-    public String getMarginTest ()  {
-        log.info("getMarginTest()");
-
-        return "csstest/eighth/marginTest";
-    }
-
-    @GetMapping("idSelectTest")
-    public String getIdSelectTest   ()  {
-        log.info("getIdSelectTest()");;
+    @GetMapping("/idSelectTest")
+    public String getIdSelectTest () {
+        log.info("getIdSelectTest()");
 
         return "csstest/eighth/idSelectTest";
     }
 
     @GetMapping("/allTest")
-    public String getAllTest() {
+    public String getAllTest () {
         log.info("getAllTest()");
 
         return "csstest/eighth/allTest";
     }
 
     @GetMapping("/classSelectTest")
-    public String getClassSelectTest()  {
+    public String getClassSelectTest () {
         log.info("getClassSelectTest()");
 
         return "csstest/eighth/classSelectTest";
     }
+
     @GetMapping("/tagClassSelectTest")
-    public String getTagClassSelectTest()  {
+    public String getTagClassSelectTest () {
         log.info("getTagClassSelectTest()");
 
         return "csstest/eighth/tagClassSelectTest";
