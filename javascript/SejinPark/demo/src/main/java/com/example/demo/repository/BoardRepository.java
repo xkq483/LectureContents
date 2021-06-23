aeec0e20c692d9fad746f75615afa23c9e55db0e
ethanlinje <ethanlinje@gmail.com>
2021-06-23 12:54:26 +0900

    [박세진] 32일차 복습

--- javascript/SejinPark/demo/src/main/java/com/example/demo/repository/BoardRepository.java
+++ javascript/SejinPark/demo/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -1,17 +1,14 @@
 package com.example.demo.repository;
-
 import com.example.demo.entity.Board;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-
 @Repository
 public class BoardRepository {
     @Autowired
@@ -47,22 +44,44 @@ public class BoardRepository {
                         board.setContent(rs.getString("content"));
                         board.setWriter(rs.getString("writer"));
                         // rs.getDate()는 DB에 있는 날자 정보를 얻어옴
-                        //board.setRegDate(rs.getDate("reg_date"));
                         // board.setRegDate(rs.getDate("reg_date"));
-
                         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-
                         board.setRegDate(sdf.parse(rs.getDate("reg_date") + " " + rs.getTime("reg_date")));
-
                         //System.out.println("rs.getDate(): " + rs.getTimestamp("reg_date"));
                         //System.out.println("rs.getDate(): " + rs.getDate("reg_date"));
                         //System.out.println("rs.getTime(): " + rs.getTime("reg_date"));
-
                         return board;
                     }
                 }
         );
+
         return results;
+    }
+
+    public Board read (Integer boardNo) throws Exception {
+        List<Board> results = jdbcTemplate.query(
+                "select board_no, title, content, writer, reg_date from board where board_no = ?",
+                new RowMapper<Board>() {
+                    @Override
+                    public Board mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Board board = new Board();
+
+                        board.setBoardNo(rs.getInt("board_no"));
+                        board.setTitle(rs.getString("title"));
+                        board.setContent(rs.getString("content"));
+                        board.setWriter(rs.getString("writer"));
+                        board.setRegDate(rs.getDate("reg_date"));
+
+                        return board;
+                    }
+                }, boardNo);
+
+        return results.isEmpty() ? null : results.get(0);
+    }
+
+    public void delete(Integer boardNo) throws Exception {
+        String query = "delete from board where board_no = ?";
+
+        jdbcTemplate.update(query, boardNo);
     }
 }
