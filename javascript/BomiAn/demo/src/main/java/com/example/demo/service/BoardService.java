ff9d82718560d29cb2a51ffa55c9136fd69e9700
BomiAn <qhal4686@gmail.com>
2021-06-23 22:04:44 +0900

    [안보미] - 33일차 수업내용 정리 (CSS 기본)

--- javascript/BomiAn/demo/src/main/java/com/example/demo/service/BoardService.java
+++ javascript/BomiAn/demo/src/main/java/com/example/demo/service/BoardService.java
@@ -14,4 +14,6 @@ public interface BoardService {
     public Board read(Integer boardNo) throws Exception;
 
     public void remove(Integer boardNo) throws Exception;
+
+    public void modify(Board board) throws Exception;
 }
