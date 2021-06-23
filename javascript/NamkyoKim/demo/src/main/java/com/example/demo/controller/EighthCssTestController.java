957c7cc7067efc4976595efaaa6fdb7cf56a4008
siary11 <siary11@naver.com>
2021-06-23 17:05:29 +0900

    [김남교] 33일차 CSS 테스트( 수업내용 )

---
+++ javascript/NamkyoKim/demo/src/main/java/com/example/demo/controller/EighthCssTestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+// 해당 컨트롤러를 사용할 때 무조건 URL 위치가 기본적으로 /css가 들어감
+@RequestMapping(value = "/css")
+public class EighthCssTestController {
+
+    // 결국 애는 /css/basicTest 와 같음
+    @GetMapping("/basicTest")
+    public String getBasicCssTest(){
+        log.info("getBasicCssTest()");
+
+        return "csstest/eighth/cssTest";
+    }
+
+    @GetMapping("/styleTest")
+    public String getStyleTest(){
+        log.info("getStyleTest()");
+
+        return "csstest/eighth/styleTest";
+    }
+}
