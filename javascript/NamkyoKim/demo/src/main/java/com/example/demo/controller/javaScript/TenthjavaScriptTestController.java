d5520cfbf3e3427843f5b9135967999ffd380b74
siary11 <siary11@naver.com>
2021-06-25 18:18:27 +0900

    [김남교] 35일차 JavaScript로 비동기 처리하기(수업내용)

---
+++ javascript/NamkyoKim/demo/src/main/java/com/example/demo/controller/javaScript/TenthjavaScriptTestController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller.javaScript;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class TenthjavaScriptTestController {
+
+    @GetMapping("getJavaScriptPromptTest")
+    public String getJavaScriptPromptTest(){
+        log.info("getJavaScriptPromptTest()");
+
+        return "/javascript/tenth/prompt";
+    }
+
+    @GetMapping("getQuerySelectTest")
+    public String getQuerySelectTest(){
+        log.info("getQuerySelectTest()");
+
+        return "/javascript/tenth/querySelect";
+    }
+
+    @GetMapping("/getDivideElementTest")
+    public String getDivideElementTest(){
+        log.info("getDivideElementTest()");
+
+        return "/javascript/tenth/divideElement";
+    }
+
+    @GetMapping("/getJavaScriptTimeTest")
+    public String getJavaScriptTimeTest(){
+        log.info("getJavaScriptTimeTest()");
+
+        return "/javascript/tenth/jsTime";
+    }
+
+    @GetMapping("/getSaleProductTest")
+    public String getSaleProductTest(){
+        log.info("getSaleProductTest()");
+
+        return "/javascript/tenth/saleProduct";
+    }
+}
