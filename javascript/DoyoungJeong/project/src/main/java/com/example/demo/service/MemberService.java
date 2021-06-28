f3e7d25539d3ac2bebbaa909a2e5537b118ab1e3
jdy8739 <jdy8739@naver.com>
2021-06-28 14:44:23 +0900

    0628 숙제 로그인부분 구현

--- javascript/DoyoungJeong/project/src/main/java/com/example/demo/service/MemberService.java
+++ javascript/DoyoungJeong/project/src/main/java/com/example/demo/service/MemberService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface MemberService {
     public void memberSignup(Member member) throws Exception;
     public List<Member> list() throws Exception;
+    public Boolean loginCheck(Member member) throws Exception;
 }
