d3113e7475ad0eac66d35bc1ee904946b51c1417
yoo-jong-hyeon <gowert612@gmail.com>
2021-06-29 15:33:22 +0900

    [유종현] 36일차 수업복습

--- javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/service/MemberService.java
+++ javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/service/MemberService.java
@@ -2,7 +2,10 @@ package com.example.jsmain.service;
 
 import com.example.jsmain.entity.Member;
 
+import java.util.List;
+
 public interface MemberService {
     public void register(Member member) throws Exception;
     public void login(Member member) throws Exception;
+    public List<Member> list() throws Exception;
 }
