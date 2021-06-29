608eaf451da9682e0125dddb637643044335902c
limcholong <limcho1ong@naver.com>
2021-06-29 13:52:40 +0900

    36일차 : 복습

--- javascript/CholongLim/cholongtest/src/main/resources/static/js/bank.js
+++ javascript/CholongLim/cholongtest/src/main/resources/static/js/bank.js
@@ -1,23 +1,13 @@
-
 var myMoney = 1000000;
-var interest = myMoney * ( 4 / 100 );
-var resultPrice = myMoney + interest;
+var interest = 0.04;
 
 document.querySelector('#accent').innerText = myMoney + "원";
-
 calcYear(1);
 calcYear(3);
 calcYear(10);
 
-function calcYear(year) {
-
-    var future = myMoney + interest * (1000 * 60 * 60 * 24);
-    var someday = new Date(future);
-    for ( var i = 1; i <= 10 ; i++){
-        interest[i] = myMoney[i] * ( 4 / 100 );
-        myMoney[i] += interest[i];
-        document.write(myMoney[1],myMoney[3],myMoney[10]);
-        document.querySelector('#money' + year).innerText =
-             myMoney[i];
-    }
+function calcYear (years) {
+    var nowMoney = myMoney * ( 1 + (interest * years));
+    document.querySelector("#money" + years).innerText = nowMoney + "원";
+}
 
