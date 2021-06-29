6f7c9f5ee71f71beadc48f45edc24d66394f92d8
injun0607 <injun0607@naver.com>
2021-06-29 14:20:44 +0900

    [여인준]36일차 연이자 계산 숙제

--- javascript/InjunYeo/demo/src/main/resources/static/js/bank.js
+++ javascript/InjunYeo/demo/src/main/resources/static/js/bank.js
@@ -1,36 +1,19 @@
-//오늘 날자 정보
-var now = new Date();
+//현재 가지고있는 돈
+var money = 100;
 
-//처음 만난 날자 정보
-var firstDay = new Date("2015-03-29");
 
-//오늘 날자 정보를 컴퓨터가 계산하는 숫자 정보로 변환(정확히는 ms단위로 바뀜)
-var toNow = now.getTime();
+document.querySelector('#accent').innerText = money+"만원";
 
-//처음만난 날자를 컴퓨터가 게산하는 숫자 정보로 변환
-var toFirst = firstDay.getTime();
+calcmoney(1);
+calcmoney(3);
+calcmoney(10);
 
-//오늘 날자 - 첫만남
-var passedTime = toNow - toFirst;
+function calcmoney(days){
 
-//다시 날자로 변환
-//math.round(반올림)
-var passedDay = Math.round(passedTime / (24 * 60 * 60 * 1000))+1;
+    for(var i = 0; i<days;i++){
+        money +=money*0.04;
 
-document.querySelector('#accent').innerText = passedDay+"일";
+    }
 
-calcDate(100);
-calcDate(200);
-calcDate(300);
-calcDate(400);
-
-function calcDate(days){
-    var future = toFirst+days*(1000*60*60*24);
-    var someday = new Date(future);
-
-    var year = someday.getFullYear();
-    var month = someday.getMonth()+1;
-    var date = someday.getDate();
-
-    document.querySelector('#date' + days).innerText =year+"년"+month+"월" +days+"일";
+    document.querySelector('#money' + days).innerText = Math.round(money)+"만원";
 }
