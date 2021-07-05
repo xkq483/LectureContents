d94eec9ff3f20cc4f0f1af9dc134738012e226ef
bombom23 <tkddn5678@naver.com>
2021-07-05 15:29:00 +0900

    [한상우] 7월 2일 vue 복습

--- javascript/SangwooHan/demo/src/main/resources/static/vue/second/game.js
+++ javascript/SangwooHan/demo/src/main/resources/static/vue/second/game.js
@@ -4,9 +4,27 @@ el: '#game',
         randomNumber: 0 ,
         MonsterRandomHp: 0,
         monsterName: '',
+        randomExp:0,
         dotory: '도토리',
         message2: '나의인벤토리',
     message: '몬스터 도감',
+
+    characterStatus: {
+        level: 1,
+        hp: 75,
+        mp: 30,
+        atk: 10,
+        str: 10,
+        int: 10,
+        dex: 10,
+        vit: 10,
+        def: 10,
+        men: 0,
+        // 전체 필요 경험치량
+        totalLevelBar: 10,
+        currentLevelBar: 0,
+        // 현재 누적한 경험치량
+    },
     list: ['[고블린]','[황금고블린]','[도깨비]','[사나운들쥐]','[주인없는고양이]','[황금쥐]','[도토리다람쥐]','[독버섯]',
         '[사나운들개]','[부서진피아노]','[거대한말벌]','[바나나킹콩]','[바나나잃은킹콩]','[두둥실풍선]',
         '[폭력적인원숭이] ','[위험한곰]','[새끼곰]','[꿀벌] '],
@@ -17,9 +35,9 @@ el: '#game',
         ],
 
     monsters: [
-        {id: 1, name3:'썬더`전설`', hp:9999},
-        {id: 2, name3:'파이어`전설`', hp:9999},
-       {id: 3, name3:'프리저`전설`', hp: 9999},
+        {id: 1, name3:'썬더`전설`', hp:9999, exp:99999},
+        {id: 2, name3:'파이어`전설`', hp:9999, exp:99999},
+       {id: 3, name3:'프리저`전설`', hp: 9999, exp:99999},
    ],
 
 
@@ -37,7 +55,10 @@ el: '#game',
         this.monsters.push({
             id: max +1,
             name3: this.list[this.randomNumber =Math.floor(Math.random() * 17)+1],
-            hp: this.MonsterRandomHp = Math.floor(Math.random() * 551)+100
+            hp: this.MonsterRandomHp = Math.floor(Math.random() * 40)+10,
+            exp: this.randomExp = Math.floor(Math.random() * 4)+1,
+            
+            
         })
     },
     removeMonster: function (index) {
@@ -48,7 +69,7 @@ el: '#game',
 
   userAttack: function (index){
 
-     this.monsters[index].hp -=30
+     this.monsters[index].hp -=this.characterStatus.atk
 
   },
 
@@ -68,11 +89,13 @@ beforeUpdate() {
                     this.inventory.push({
                         item: '도토리',
                     })
+                    this.characterStatus.currentLevelBar += this.monsters
             }
            }
         },
 
 
 randomNumber: 0,
-MonsterRandomHp :0
+MonsterRandomHp :0,
+randomExp: 0
     })
