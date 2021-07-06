c1f455a292a2e7eee16df758fd6face4b227b31f
ChoiJunHwanzz <wnsghks884@gmail.com>
2021-07-06 16:08:01 +0900

    [최준환]41일차 복습

---
+++ javascript/JunhwanChoi/jswithspring/src/main/resources/static/js/vue/fourth/fourth.js
@@ -0,0 +1,37 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        val: true,
+        groupVal: [],
+        radioGroupVal:'',
+        selectVal:[],
+        selectOnce: ''
+    },
+    methods: {
+
+    },
+    beforeCreate() {
+        console.log('Vue 객체를 만들기 이전입니다.')
+    },
+    created() {
+        console.log('Vue 객체를 만들었습니다.')
+    },
+    beforeMount() {
+        console.log('HTML 요소를 붙이기 전입니다.')
+    },
+    mounted() {
+        console.log('HTML 요소를 붙입니다.')
+    },
+    beforeUpdate() {
+        console.log('VDOM의 변화를 감지합니다.')
+    },
+    updated() {
+        console.log('VDOM의 변화를 적용합니다.')
+    },
+    beforeDestroy() {
+        console.log('Vue 객체를 파괴하기 이전입니다.')
+    },
+    destroyed() {
+        console.log('Vue 객체를 파괴하였습니다.')
+    }
+})
