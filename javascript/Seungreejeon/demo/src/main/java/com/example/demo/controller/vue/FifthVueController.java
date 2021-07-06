d265ed21f6e35d699d8bc46ed7f97d6338cb274a
JEONSEUNGREE <jws08219@naver.com>
2021-07-06 22:03:14 +0900

    [전승리]41일차 복습

---
+++ javascript/Seungreejeon/demo/src/main/java/com/example/demo/controller/vue/FifthVueController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller.vue;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class FifthVueController {
+
+    @GetMapping("/fifthVue")
+    public String getFourthVue() {
+        log.info("fifthVue()");
+
+        return "/vue/fifthVue";
+    }
+}
