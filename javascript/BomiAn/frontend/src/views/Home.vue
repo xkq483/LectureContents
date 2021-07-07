d92bd0af06165baad15986245e77b06004a44eac
BomiAn <qhal4686@gmail.com>
2021-07-07 23:24:43 +0900

    [안보미] - 43일차 vue setting

---
+++ javascript/BomiAn/frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        <div>
+            <router-link :to="{ name: 'Home' }"
+                    class="nav-link"
+                    active-class="active">
+                Home
+            </router-link>
+            <router-link :to="{ name: 'Test' }"
+                    class="nav-link"
+                    active-class="active">
+                Test
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
