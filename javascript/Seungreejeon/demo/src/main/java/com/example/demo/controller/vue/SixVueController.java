17c5018550868272f92b03dba4d8b888e86b7ab0
JEONSEUNGREE <jws08219@naver.com>
2021-07-07 19:29:51 +0900

    test

---
+++ javascript/Seungreejeon/demo/src/main/java/com/example/demo/controller/vue/SixVueController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller.vue;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class SixVueController {
+
+    @GetMapping("/sixVue")
+    public String getSixVue() {
+        log.info("getSixVue()");
+
+        return "/vue/sixth";
+    }
+}
