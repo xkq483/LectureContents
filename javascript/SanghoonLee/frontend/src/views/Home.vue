c17b5d2abe77b730208851de7a5ee579312e3a9d
silenc3502 <gcccompil3r@gmail.com>
2021-07-07 21:03:11 +0900

    43일차: Vue CLI 라우터 설정

--- javascript/SanghoonLee/frontend/src/views/Home.vue
+++ javascript/SanghoonLee/frontend/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="home">
-    <router-link :to="{ name: 'Home' }"
-        class="nav-link"
-        active-class="active">
-      Home
-    </router-link>
-
-  </div>
+    <div class="home">
+        <div>
+            <router-link :to="{ name: 'Home' }"
+                    class="nav-link"
+                    active-class="active">
+                Home
+            </router-link>
+            <router-link :to="{ name: 'Test' }"
+                    class="nav-link"
+                    active-class="active">
+                Test
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
