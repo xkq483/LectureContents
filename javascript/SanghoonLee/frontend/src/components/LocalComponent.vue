c214a96fb0ee54b6ccfdd9a59753b9d32e2078f7
silenc3502 <gcccompil3r@gmail.com>
2021-07-08 19:33:58 +0900

    44일차: Vue 3.x 지역 Component 등록

---
+++ javascript/SanghoonLee/frontend/src/components/LocalComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- 실제 해당 컴포넌트를 사용하는 측에서
+         v-bind:num="블라블라" 형식으로 맵핑됨 -->
+    <div>{{ num }}</div>
+</template>
+
+<script>
+
+export default {
+    name: 'LocalComponent',
+    props: {
+        num: Number
+    }
+}
+
+</script>
