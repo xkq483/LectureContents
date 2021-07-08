f2f2145f58e2b1470c591e0dda88c1c2ff24d850
silenc3502 <gcccompil3r@gmail.com>
2021-07-08 19:30:58 +0900

    44일차: Vue 3.x 지역 Component 등록

--- javascript/SanghoonLee/frontend/src/views/Test.vue
+++ javascript/SanghoonLee/frontend/src/views/Test.vue
@@ -23,21 +23,32 @@
 
             <global-component v-bind:initial-test="counter">
             </global-component>
+
+            <local-component v-bind:num="value"></local-component>
+            <button v-on:click="plus">클릭해봐!</button>
         </div>
     </div>
 </template>
 
 <!-- javascript 관련 파트 작성 -->
 <script>
+import LocalComponent from '../components/LocalComponent.vue'
 
 export default {
+    components: {
+        'local-component': LocalComponent
+    },
     data () {
         return {
             counter: 0,
+            value: 0,
             msg: 'Vue 객체 데이터'
         }
     },
     methods: {
+        plus () {
+            this.value++
+        }
     }
 }
 
