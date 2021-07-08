b0e6c3d58b46d4c9244cf35ec635a9469007776b
silenc3502 <gcccompil3r@gmail.com>
2021-07-08 20:31:36 +0900

    44일차: Vue 2.x Vuetify 기반 Component 활용

--- javascript/SanghoonLee/front_downgrade/src/views/Home.vue
+++ javascript/SanghoonLee/front_downgrade/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div id="header">
+            <router-link :to="{ name: 'Home' }"
+                    class="nav-link"
+                    active-class="active">
+                Home
+            </router-link>
+            <router-link :to="{ name: 'Test' }"
+                    class="nav-link"
+                    active-class="active">
+                Test
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name: 'Home',
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    components: {
+    },
   }
-}
 </script>
