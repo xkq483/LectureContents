28a9cb82d82b5f195101a027e91cb0e9a5ebab52
BomiAn <qhal4686@gmail.com>
2021-07-08 17:55:53 +0900

    apply modification

--- javascript/BomiAn/frontend/src/views/Home.vue
+++ javascript/BomiAn/frontend/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="home">
-        <div>
+        <div id="header">
+            <!-- router-link는 어디로 갈지 URL 맵핑해주는 Spring의 Controller 같은 역할
+                 to: Home은 이 녀석을 클릭했을 때 router/index.js에 등록된 Home으로 가게 만듬 
+                 class: nav-link의 경우 Navigation Bar 같은 역할을 해줄 수 있게 구성함을 의미
+                 active-class: 링크가 활성화 되었을때 적용된 CSS를 의미함 -->
             <router-link :to="{ name: 'Home' }"
                     class="nav-link"
                     active-class="active">
@@ -16,10 +20,9 @@
 </template>
 
 <script>
-// @ is an alias to /src
-export default {
-  name: 'Home',
-  components: {
+  export default {
+    name: 'Home',
+    components: {
+    },
   }
-}
 </script>
