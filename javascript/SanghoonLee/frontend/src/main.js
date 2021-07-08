dc14315f1be6ad075974a70c389951fb56e5ac93
silenc3502 <gcccompil3r@gmail.com>
2021-07-08 17:33:18 +0900

    44일차: Vue 3.x 전역 Component 등록

--- javascript/SanghoonLee/frontend/src/main.js
+++ javascript/SanghoonLee/frontend/src/main.js
@@ -1,6 +1,27 @@
 import { createApp } from 'vue'
+// import {Vue} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import vuetify from './plugins/vuetify'
+import GlobalComponent from './components/GlobalComponent.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp()을 통해 Vue 객체를 생성함 
+const app = createApp(App)
+
+// 실제 Vue 객체를 생성하는 중앙(main)에서 사용할 컴포넌트의 등록까지 모두 처리함
+// 가장 최근 Vue (7월 1일)의 경우 컴포넌트 등록을 중앙에서 하도록 변경함
+// 중앙에서 컴포넌트를 등록하는 방법은 아래와 같이
+// app.component(컴포넌트 이름, 연결되는 실제 컴포넌트 객체) 형식으로 등록할 수 있다.
+// 밑에 붙어 있는 use(store).use(router).mount('#app')은 향후 Vuex 까지 잠시 보류한다.
+app.component(GlobalComponent.name, GlobalComponent)
+.use(store).use(router).mount('#app')
+
+/*
+new Vue ({
+    router,
+    store,
+    // vuetify,
+    render: h => h(App)
+}).$mount('#app')
+*/
--- javascript/SanghoonLee/frontend/src/views/Test.vue
+++ javascript/SanghoonLee/frontend/src/views/Test.vue
@@ -1,6 +1,11 @@
+<!-- HTML 요소 파트 작성 - Vue랑 연결 시킬 형식으로 작성해도 무방 -->
 <template>
     <div class="home">
-        <div>
+        <div id="header">
+            <!-- router-link는 어디로 갈지 URL 맵핑해주는 Spring의 Controller 같은 역할
+                 to: Home은 이 녀석을 클릭했을 때 router/index.js에 등록된 Home으로 가게 만듬 
+                 class: nav-link의 경우 Navigation Bar 같은 역할을 해줄 수 있게 구성함을 의미
+                 active-class: 링크가 활성화 되었을때 적용된 CSS를 의미함 -->
             <router-link :to="{ name: 'Home' }"
                     class="nav-link"
                     active-class="active">
@@ -13,17 +18,23 @@
             </router-link>
         </div>
         <h2>테스트 페이지</h2>
+        <div id="app">
+            <p>{{ msg }}</p>
+
+            <global-component v-bind:initial-test="counter">
+            </global-component>
+        </div>
     </div>
 </template>
 
+<!-- javascript 관련 파트 작성 -->
 <script>
 
 export default {
-    components: {
-    },
     data () {
         return {
-            msg: 'Test'
+            counter: 0,
+            msg: 'Vue 객체 데이터'
         }
     },
     methods: {
