12b01cb7347da88dc8601f68f57f217e7d6f3b41
silenc3502 <gcccompil3r@gmail.com>
2021-07-09 17:18:02 +0900

    45일차: Spring(PathVariable) + HttpMethod(GET, POST) 진행중

---
+++ javascript/SanghoonLee/jswithspring/src/main/java/com/example/jswithspring/controller/springtest/HttpMethodMainController.java
@@ -0,0 +1,17 @@
+package com.example.jswithspring.controller.springtest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HttpMethodMainController {
+
+    @GetMapping("/main")
+    public String getMain () {
+        log.info("getMain()");
+
+        return "httpmethod/main";
+    }
+}
